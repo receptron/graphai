2b0ef80e2d57d7008ea746e653d425dbccc9eddf
snakajima <satoshi.nakajima@gmail.com>
2024-04-05 14:30:19 -0700

    file_utils.ts

---
+++ src/file_utils.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+import YAML from "yaml";
+
+export const readManifestData = (file: string) => {
+  if (file.endsWith(".yaml") || file.endsWith(".yml")) {
+    return readYamlManifest(file);
+  }
+  if (file.endsWith(".json")) {
+    return readJsonManifest(file);
+  }
+  throw new Error("No file exists " + file);
+};
+
+export const readJsonManifest = (fileName: string) => {
+  const manifest_file = fs.readFileSync(fileName, "utf8");
+  const manifest = JSON.parse(manifest_file);
+  return manifest;
+};
+
+export const readYamlManifest = (fileName: string) => {
+  const manifest_file = fs.readFileSync(fileName, "utf8");
+  const manifest = YAML.parse(manifest_file);
+  return manifest;
+};
