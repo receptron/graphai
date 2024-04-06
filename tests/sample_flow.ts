3ba92c196865b1e7e9b172e2d0afbf77bebbf0fe
isamu <isamu@to-kyo.to>
2024-04-07 05:39:16 +0900

    node:test

--- tests/sample_flow.ts
+++ tests/sample_flow.ts
@@ -2,7 +2,10 @@ import path from "path";
 import { GraphAI } from "../src/graphai";
 import { readManifestData } from "../src/file_utils";
 
-const test = async (file: string) => {
+import test from "node:test";
+import assert from "node:assert";
+
+const runTest = async (file: string) => {
   const file_path = path.resolve(__dirname) + file;
   const graph_data = readManifestData(file_path);
   const graph = new GraphAI(graph_data, async (nodeId, transactionId, retry, params, payload) => {
@@ -22,12 +25,28 @@ const test = async (file: string) => {
 
   const results = await graph.run();
   console.log(results);
+  return results;
 };
 
-const main = async () => {
-  await test("/graphs/sample1.yml");
-  console.log("COMPLETE 1");
-  await test("/graphs/sample2.yml");
+test("test sample1", async () => {
+  const result = await runTest("/graphs/sample1.yml");
+  assert.deepStrictEqual(result, {
+    node1: { node1: 'output' },
+    node2: { node2: 'output' },
+    node3: { node3: 'output' },
+    node4: { node4: 'output' },
+    node5: { node5: 'output' }
+  });
+});
+
+test("test sample1", async () => {
+  const result = await runTest("/graphs/sample2.yml");
+  assert.deepStrictEqual(result, {
+    node1: { node1: 'output' },
+    node2: { node2: 'output' },
+    node3: { node3: 'output' },
+    node4: { node4: 'output' },
+    node5: { node5: 'output' }
+  });
   console.log("COMPLETE 2");
-};
-main();
+});
