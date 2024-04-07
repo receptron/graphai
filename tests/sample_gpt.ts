59b7a400d825bcfbf98060fc116902054812b280
snakajima <satoshi.nakajima@gmail.com>
2024-04-06 20:53:50 -0700

    testFunction

--- tests/sample_gpt.ts
+++ tests/sample_gpt.ts
@@ -5,26 +5,28 @@ import { readManifestData } from "../src/file_utils";
 
 const config = new ChatConfig(path.resolve(__dirname));
 
+const testFunction = async (context: NodeExecuteContext) => {
+  console.log("executing", context.nodeId, context.params, context.payload);
+  const session = new ChatSession(config, context.params.manifest ?? {});
+  const prompt = Object.keys(context.payload).reduce((prompt, key) => {
+    return prompt.replace("${" + key + "}", context.payload[key]["answer"]);
+  }, context.params.prompt);
+  session.append_user_question(prompt);
+
+  await session.call_loop(() => {});
+  const message = session.history.last_message();
+  if (message === undefined) {
+    throw new Error("No message in the history");
+  }
+  const result = { answer: message.content };
+  console.log(result);
+  return result;
+};
+
 const test = async (file: string) => {
   const file_path = path.resolve(__dirname) + file;
   const graph_data = readManifestData(file_path);
-  const graph = new GraphAI(graph_data, async (context) => {
-    console.log("executing", context.nodeId, context.params, context.payload);
-    const session = new ChatSession(config, context.params.manifest ?? {});
-    const prompt = Object.keys(context.payload).reduce((prompt, key) => {
-      return prompt.replace("${" + key + "}", context.payload[key]["answer"]);
-    }, context.params.prompt);
-    session.append_user_question(prompt);
-
-    await session.call_loop(() => {});
-    const message = session.history.last_message();
-    if (message === undefined) {
-      throw new Error("No message in the history");
-    }
-    const result = { answer: message.content };
-    console.log(result);
-    return result;
-  });
+  const graph = new GraphAI(graph_data, testFunction);
   await graph.run();
 };
 
