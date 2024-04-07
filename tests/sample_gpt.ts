2c29a789f02dfd17d3ac0986039d029e93897559
snakajima <satoshi.nakajima@gmail.com>
2024-04-06 20:51:02 -0700

    NodeExecuteContext

--- tests/sample_gpt.ts
+++ tests/sample_gpt.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import { GraphAI } from "../src/graphai";
+import { GraphAI, NodeExecuteContext } from "../src/graphai";
 import { ChatSession, ChatConfig } from "slashgpt";
 import { readManifestData } from "../src/file_utils";
 
@@ -8,12 +8,12 @@ const config = new ChatConfig(path.resolve(__dirname));
 const test = async (file: string) => {
   const file_path = path.resolve(__dirname) + file;
   const graph_data = readManifestData(file_path);
-  const graph = new GraphAI(graph_data, async (nodeId, retry, params, payload) => {
-    console.log("executing", nodeId, params, payload);
-    const session = new ChatSession(config, params.manifest ?? {});
-    const prompt = Object.keys(payload).reduce((prompt, key) => {
-      return prompt.replace("${" + key + "}", payload[key]["answer"]);
-    }, params.prompt);
+  const graph = new GraphAI(graph_data, async (context) => {
+    console.log("executing", context.nodeId, context.params, context.payload);
+    const session = new ChatSession(config, context.params.manifest ?? {});
+    const prompt = Object.keys(context.payload).reduce((prompt, key) => {
+      return prompt.replace("${" + key + "}", context.payload[key]["answer"]);
+    }, context.params.prompt);
     session.append_user_question(prompt);
 
     await session.call_loop(() => {});
