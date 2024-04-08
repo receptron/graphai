edebcfc2b8479a66346eddc9a61f6a420ad2408f
isamu <isamu@to-kyo.to>
2024-04-09 08:49:22 +0900

    paper agent

---
+++ samples/agents/slashgpt.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { NodeExecute } from "@/graphai";
+import { ChatSession, ChatConfig, ManifestData } from "slashgpt";
+
+const config = new ChatConfig(path.resolve(__dirname));
+
+export const slashGPTAgent: NodeExecute<{ manifest: ManifestData; prompt: string }, { answer: string }> = async (context) => {
+  console.log("executing", context.nodeId, context);
+  const session = new ChatSession(config, context.params?.manifest ?? {});
+  
+  const prompt = [context.params?.prompt, context.payload.arxiv2TextAgent].join("\n\n");
+  session.append_user_question(prompt);
+
+  await session.call_loop(() => {});
+  const message = session.history.last_message();
+  if (message === undefined) {
+    throw new Error("No message in the history");
+  }
+  const result = { answer: message.content };
+  return result;
+};
+
