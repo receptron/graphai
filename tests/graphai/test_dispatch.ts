a1a7f44bbd38cb1bb3d3ec510afa1d1a52d7c43b
snakajima <satoshi.nakajima@gmail.com>
2024-04-08 21:55:36 -0700

    logs

--- tests/graphai/test_dispatch.ts
+++ tests/graphai/test_dispatch.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { GraphAI, NodeExecute } from "@/graphai";
 import { readGraphaiData } from "~/file_utils";
 import { sleep } from "~/utils";
+import * as fs from 'fs';
 
 import test from "node:test";
 import assert from "node:assert";
@@ -52,12 +53,14 @@ const dispatchFunction: NodeExecute<{ delay: number; fail: boolean }> = async (c
 
 const runTest = async (file: string, callback:(graph:GraphAI) => void = ()=>{}) => {
   const file_path = path.resolve(__dirname) + "/.." + file;
+  const log_path = path.resolve(__dirname) + "/../logs/" + path.basename(file_path).replace(/\.yml$/, ".log");
   const graph_data = readGraphaiData(file_path);
   const graph = new GraphAI(graph_data, {default: testFunction, alt: dispatchFunction});
   callback(graph);
 
   try {
     const results = await graph.run();
+    fs.writeFileSync(log_path, JSON.stringify(graph.transactionLogs(), null, 2));
     // console.log(graph.transactionLogs());
     return results;
   } catch (error) {
