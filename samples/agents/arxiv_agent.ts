451f1e3858015ec1bbc93f3ccbe8c33534310eaf
isamu <isamu@to-kyo.to>
2024-04-09 11:21:34 +0900

    arxiv_agent

---
+++ samples/agents/arxiv_agent.ts
@@ -0,0 +1,46 @@
+import search from "arXiv-api-ts";
+
+import { NodeExecute } from "@/graphai";
+
+type arxivData = { id: string; title: string; summary: string };
+
+const search_arxiv_papers = async (keywords: string[], limit = 10) => {
+  const includes = keywords.map((k) => {
+    return { name: k };
+  });
+  const papers = await search({
+    searchQueryParams: [
+      {
+        include: includes,
+      },
+    ],
+    sortBy: "lastUpdatedDate",
+    sortOrder: "descending",
+    start: 0,
+    maxResults: limit,
+  });
+  return papers.entries || [];
+};
+
+export const arxivAgent: NodeExecute<{ keywords: string[]; limit: number }, arxivData[]> = async (context) => {
+  const { keywords, limit } = context.params;
+  const arxivResult = await search_arxiv_papers(keywords, limit);
+  // console.log("executing", arxivResult, context.params.keywords);
+
+  const result = arxivResult.map((r: any) => {
+    const { id, title, summary } = r;
+    return { id, title, summary };
+  });
+  return result;
+};
+
+export const arxiv2TextAgent: NodeExecute<{}, string, string[]> = async (context) => {
+  const result = (context?.payload?.inputData || [])
+    .map((r: any) => {
+      const { id, title, summary } = r;
+      return ["id:", id, "title:", title, "summary:", summary].join("\n");
+    })
+    .join("\n\n\n");
+
+  return result;
+};
