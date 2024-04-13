9a69f36374c7ee8f6d6e30962cd50f3af6a5638d
isamu <isamu@to-kyo.to>
2024-04-13 09:26:40 +0900

    inputKey

--- src/experimental_agents/string_agent.ts
+++ src/experimental_agents/string_agent.ts
@@ -1,9 +1,9 @@
 import { AgentFunction } from "@/graphai";
 
-export const stringTemplateAgent: AgentFunction<{ template: string }, { content: string }> = async (context) => {
+export const stringTemplateAgent: AgentFunction<{ template: string; inputKey?: string }, { content: string }> = async (context) => {
   console.log("executing", context.nodeId, context.params);
   const content = context.inputs.reduce((template, input, index) => {
-    return template.replace("${" + index + "}", input["content"]);
+    return template.replace("${" + index + "}", input[context.params.inputKey ?? "content"]);
   }, context.params.template);
 
   return { content };
--- tests/agents/test_string_agent.ts
+++ tests/agents/test_string_agent.ts
@@ -3,12 +3,24 @@ import { stringTemplateAgent } from "@/experimental_agents";
 import test from "node:test";
 import assert from "node:assert";
 
-test("test stringTemplateAgent", async () => {
+test("test stringTemplateAgent simple", async () => {
   const result = await stringTemplateAgent({
     nodeId: "test",
     retry: 0,
     params: { template: "${0}: ${1}" },
     inputs: [{ content: "hello" }, { content: "test" }] as any,
+  });
+  assert.deepStrictEqual(result, {
+    content: "hello: test",
+  });
+});
+
+test("test stringTemplateAgent simple", async () => {
+  const result = await stringTemplateAgent({
+    nodeId: "test",
+    retry: 0,
+    params: { template: "${0}: ${1}", inputKey: "key" },
+    inputs: [{ key: "hello" }, { key: "test" }],
   });
   assert.deepStrictEqual(result, {
     content: "hello: test",
