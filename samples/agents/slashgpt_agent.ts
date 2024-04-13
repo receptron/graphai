1abdfa7215fd5d108c3bd2c82eadd9342ad24fdc
snakajima <satoshi.nakajima@gmail.com>
2024-04-12 21:57:21 -0700

    got rid of old slashGPTAgent

--- samples/agents/slashgpt_agent.ts
+++ samples/agents/slashgpt_agent.ts
@@ -17,34 +17,3 @@ export const slashGPTFuncitons2TextAgent: AgentFunction<
 
   return { content: result[context.forkIndex ?? 0] };
 };
-
-export const slashGPTAgentObsolete: AgentFunction<
-  { manifest: ManifestData; prompt: string; function_result?: boolean; debug?: boolean },
-  { answer: string },
-  string
-> = async (context) => {
-  const { params } = context;
-  if (params.debug) {
-    console.log("executing", context.nodeId, context);
-  }
-  const session = new ChatSession(config, params?.manifest ?? {});
-
-  const prompt = params?.prompt ?? [context.inputs].filter((a) => a !== undefined).join("\n\n");
-  // console.log(prompt);
-  session.append_user_question(prompt);
-
-  await session.call_loop(() => {});
-
-  // console.log(session.history)
-  const message = (() => {
-    if (params.function_result) {
-      return session.history.messages().find((m) => m.role === "function_result");
-    }
-    return session.history.last_message();
-  })();
-  if (message === undefined) {
-    throw new Error("No message in the history");
-  }
-  const result = { answer: message.content, function_data: message.function_data };
-  return result;
-};
