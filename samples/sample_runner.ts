29de25237c758a7198984a927b0c487a74aa2ac1
isamu <isamu@to-kyo.to>
2024-04-14 07:59:16 +0900

    home_functions

--- samples/sample_runner.ts
+++ samples/sample_runner.ts
@@ -2,12 +2,14 @@ import { main as gpt } from "./sample_gpt";
 import { main as interaction } from "./interaction";
 import { main as sample_co2 } from "./sample_co2";
 import { main as sample_paper_ai } from "./sample_paper_ai";
+import { main as home } from "./home";
 
 const main = async () => {
   await gpt();
   await interaction();
   await sample_co2();
   await sample_paper_ai();
+  await home();
 };
 
 main();
