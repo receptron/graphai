7d0fcb4533a960aa9e5715963e3425b8d0a0bccc
snakajima <satoshi.nakajima@gmail.com>
2024-04-14 05:58:57 -0700

    agents from parent

--- src/experimental_agents/nested_agent.ts
+++ src/experimental_agents/nested_agent.ts
@@ -8,13 +8,12 @@ export const nestedAgent: AgentFunction<
     graph: GraphData;
     nodeId: string;
   }
-> = async ({ params, inputs }) => {
-  const graph = new GraphAI(params.graph, sleeperAgent); // TODO: we need to get agents from the parent
+> = async ({ params, inputs, agents }) => {
+  const graph = new GraphAI(params.graph, agents);
 
   try {
     const results = await graph.run();
     // TODO: We need to injects inputs
-    console.log("**********", results);    
     return results[params.nodeId];
   } catch (error) {
     if (error instanceof Error) {
