f33e9b86d01e14ee9d6cdccd2319775c2456109e
snakajima <satoshi.nakajima@gmail.com>
2024-04-14 04:42:23 -0700

    built

---
+++ lib/experimental_agents/data_agent.d.ts
@@ -0,0 +1,3 @@
+import { AgentFunction } from "../graphai";
+export declare const dataObjectMergeTemplateAgent: AgentFunction;
+export declare const dataSumTemplateAgent: AgentFunction<Record<string, any>, number, number>;
