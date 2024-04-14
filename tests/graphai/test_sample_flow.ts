16060ad10a4782efcea9311e4ccb6706da649fb9
snakajima <satoshi.nakajima@gmail.com>
2024-04-14 13:38:35 -0700

    id changes

--- tests/graphai/test_sample_flow.ts
+++ tests/graphai/test_sample_flow.ts
@@ -73,10 +73,10 @@ test("test source2", async () => {
 test("test nested", async () => {
   const result = await fileTestRunner("/graphs/test_nested.yml", { sleeper: sleeperAgent, nested: nestedAgent });
   assert.deepStrictEqual(result, {
-    node1: { node1: "output" },
-    node2: { node10: "output", node20: "output", node30: "output", node1: "output" },
-    node3: { node3: "output", node1: "output", node10: "output", node20: "output", node30: "output" },
-    node4: { node4: "output", node3: "output", node1: "output", node10: "output", node20: "output", node30: "output" },
-    node5: { node5: "output", node4: "output", node3: "output", node1: "output", node10: "output", node20: "output", node30: "output" },
+    outer1: { outer1: "output" },
+    outer2: { inner1: "output", inner2: "output", inner3: "output", outer1: "output" },
+    outer3: { outer3: "output", outer1: "output", inner1: "output", inner2: "output", inner3: "output" },
+    outer4: { outer4: "output", outer3: "output", outer1: "output", inner1: "output", inner2: "output", inner3: "output" },
+    outer5: { outer5: "output", outer4: "output", outer3: "output", outer1: "output", inner1: "output", inner2: "output", inner3: "output" },
   });
 });
