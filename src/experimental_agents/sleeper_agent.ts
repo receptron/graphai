600d34b1e5665d7237b35f6be540500517485d60
snakajima <satoshi.nakajima@gmail.com>
2024-04-14 03:50:06 -0700

    deep merge

--- src/experimental_agents/sleeper_agent.ts
+++ src/experimental_agents/sleeper_agent.ts
@@ -1,10 +1,11 @@
 import { AgentFunction } from "@/graphai";
 import { sleep } from "../utils/utils";
+import deepmerge from "deepmerge";
 
 export const sleeperAgent: AgentFunction<{ duration: number; result?: Record<string, any>; index?: number }> = async (context) => {
   const { params, inputs } = context;
   await sleep(params.duration);
   return inputs.reduce((result: Record<string, any>, input: Record<string, any>) => {
-    return { ...result, ...input };
+    return deepmerge(result, input);
   }, params.result ?? {});
 };
