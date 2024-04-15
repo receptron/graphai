41c19d0476fb39b948bb831e0749a94436fa7639
snakajima <satoshi.nakajima@gmail.com>
2024-04-15 10:21:27 -0700

    user interactiveInputText agent

--- samples/interaction.ts
+++ samples/interaction.ts
@@ -4,20 +4,7 @@ import { GraphAI, GraphData } from "@/graphai";
 import * as readline from "readline";
 import { mergeNodeIdAgent } from "~/agents/agents";
 import { graphDataTestRunner } from "~/utils/runner";
-
-const getUserInput = async (question: string): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-
-    rl.question(question, (answer) => {
-      rl.close();
-      resolve(answer);
-    });
-  });
-};
+import { interactiveInputTextAgent } from "./agents/interactiveInputAgent";
 
 const graph_data: GraphData = {
   nodes: {
@@ -25,17 +12,19 @@ const graph_data: GraphData = {
       source: true,
     },
     node2: {
-      inputs: ["node1"],
+      agentId: "input",
+    },
+    node3: {
+      inputs: ["node1", "node2"],
+      agentId: "merge",
     },
   },
 };
 
 export const main = async () => {
-  const query = await getUserInput("Please enter your question: ");
-  console.log("query=", query);
-  graph_data.nodes.node1.result = { query };
+  graph_data.nodes.node1.result = { injected: "test" };
 
-  const result = await graphDataTestRunner(__filename, graph_data, mergeNodeIdAgent);
+  const result = await graphDataTestRunner(__filename, graph_data, { merge: mergeNodeIdAgent, input: interactiveInputTextAgent });
   console.log(result);
 
   console.log("COMPLETE 1");
