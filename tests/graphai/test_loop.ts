553980cb52f676952d7fde6e053a5d38410702b6
snakajima <satoshi.nakajima@gmail.com>
2024-04-16 09:16:35 -0700

    pushAgent

--- tests/graphai/test_loop.ts
+++ tests/graphai/test_loop.ts
@@ -1,29 +1,42 @@
 import { AgentFunction } from "@/graphai";
 import { graphDataTestRunner } from "~/utils/runner";
-
-import { sleep } from "~/utils/utils";
-
 import { sleeperAgent } from "@/experimental_agents";
 
 import test from "node:test";
 import assert from "node:assert";
 
+const pushAgent: AgentFunction<{}, Record<string, any>, Record<string, any>> = async (context) => {
+  const { inputs } = context;
+  const [ array, item ] = inputs;
+  // TODO: Varidation
+  array.push(item);
+  return array;
+};
+
 const graph_data = {
+  loop: {
+    count: 10
+  },
   nodes: {
-    node1: {
+    array: {
+      value: []
+    },
+    item: {
       agentId: "sleeper",
       params: {
-        duration: 500,
-        value: {
-          node1: "output"
-        }
+        duration: 10,
+        value: "hello"
       }
+    },
+    reducer: {
+      agentId: "push",
+      inputs: ["array", "item"]
     }
   }
 };
 
 test("test dispatch", async () => {
-  const result = await graphDataTestRunner("test_loop", graph_data, { sleeper: sleeperAgent });
+  const result = await graphDataTestRunner("test_loop", graph_data, { sleeper: sleeperAgent, push: pushAgent });
   console.log(result);
 
   /*
