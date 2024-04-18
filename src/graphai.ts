36f0167af2873c6482465b9e8d1eafa1ddc29c84
isamu <isamu@to-kyo.to>
2024-04-19 05:12:17 +0900

    isStaticNode

--- src/graphai.ts
+++ src/graphai.ts
@@ -31,8 +31,8 @@ export class GraphAI {
 
     const nodes = Object.keys(data.nodes).reduce((_nodes: GraphNodes, nodeId: string) => {
       const fork = data.nodes[nodeId].fork;
-      const isSource = (data.nodes[nodeId].agentId ?? data.agentId) === undefined;
-      const node = isSource ? StaticNode : ComputedNode;
+      const isStaticNode = (data.nodes[nodeId].agentId ?? data.agentId) === undefined;
+      const node = isStaticNode ? StaticNode : ComputedNode;
       if (fork) {
         // For fork, change the nodeId and increase the node
         nodeId2forkedNodeIds[nodeId] = new Array(fork).fill(undefined).map((_, i) => {
