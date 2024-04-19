ca1559c6301bce6cd7d92d3acf664d995d131627
isamu <isamu@to-kyo.to>
2024-04-19 15:00:45 +0900

    more validation rule

--- src/validator.ts
+++ src/validator.ts
@@ -4,6 +4,14 @@ export const validateGraphData = (data: GraphData) => {
   if (data.nodes === undefined) {
     throw new Error("Invalid Graph Data: no nodes");
   }
-
+  if (typeof data.nodes !== "object") {
+    throw new Error("Invalid Graph Data: invalid nodes");
+  }
+  if (Array.isArray(data.nodes)) {
+    throw new Error("Invalid Graph Data: nodes must be object");
+  }
+  if (Object.keys(data.nodes).length === 0) {
+    throw new Error("Invalid Graph Data: nodes is empty");
+  }
   return true;
 };
--- tests/graphai/test_invalid_data.ts
+++ tests/graphai/test_invalid_data.ts
@@ -3,12 +3,52 @@ import { graphDataTestRunner } from "~/utils/runner";
 import test from "node:test";
 import assert from "node:assert";
 
-test("test invalid 1", async () => {
-  const graph_data = JSON.parse(JSON.stringify({}));
+const anonymization = (data: Record<string, any>) => {
+  return JSON.parse(JSON.stringify(data));
+};
+
+test("test validation no data", async () => {
+  const graph_data = anonymization({});
   await assert.rejects(
     async () => {
       await graphDataTestRunner(__filename, graph_data, {});
     },
     { name: "Error", message: "Invalid Graph Data: no nodes" },
+  );
+});
+
+test("test validation nodes is array", async () => {
+  const graph_data = anonymization({
+    nodes: [],
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, {});
+    },
+    { name: "Error", message: "Invalid Graph Data: nodes must be object" },
+  );
+});
+
+test("test validation nodes is empty", async () => {
+  const graph_data = anonymization({
+    nodes: {},
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, {});
+    },
+    { name: "Error", message: "Invalid Graph Data: nodes is empty" },
+  );
+});
+
+test("test validation nodes is not object", async () => {
+  const graph_data = anonymization({
+    nodes: "123",
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, {});
+    },
+    { name: "Error", message: "Invalid Graph Data: invalid nodes" },
   );
 });
