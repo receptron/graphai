b9a07907bf8ce64ecb4d34ccc550fe0badbf016b
isamu <isamu@to-kyo.to>
2024-04-19 16:18:42 +0900

    add test

---
+++ tests/graphai/test_validator_static_node.ts
@@ -0,0 +1,108 @@
+import { graphDataTestRunner } from "~/utils/runner";
+import { echoAgent } from "~/agents/agents";
+
+import test from "node:test";
+import assert from "node:assert";
+
+const anonymization = (data: Record<string, any>) => {
+  return JSON.parse(JSON.stringify(data));
+};
+
+test("test static node validation inputs", async () => {
+  const graph_data = anonymization({
+    nodes: {
+      static1: {
+        inputs: [""],
+      },
+    },
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, { echoAgent });
+    },
+    { name: "Error", message: "Static node does not allow inputs" },
+  );
+});
+
+test("test static node validation anyInput", async () => {
+  const graph_data = anonymization({
+    nodes: {
+      static1: {
+        anyInput: true,
+      },
+    },
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, { echoAgent });
+    },
+    { name: "Error", message: "Static node does not allow anyInput" },
+  );
+});
+
+
+test("test static node validation params", async () => {
+  const graph_data = anonymization({
+    nodes: {
+      static1: {
+        params: {},
+      },
+    },
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, { echoAgent });
+    },
+    { name: "Error", message: "Static node does not allow params" },
+  );
+});
+
+test("test static node validation retry", async () => {
+  const graph_data = anonymization({
+    nodes: {
+      static1: {
+        retry: 1,
+      },
+    },
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, { echoAgent });
+    },
+    { name: "Error", message: "Static node does not allow retry" },
+  );
+});
+
+test("test static node validation timeout", async () => {
+  const graph_data = anonymization({
+    nodes: {
+      static1: {
+        timeout: 1,
+      },
+    },
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, { echoAgent });
+    },
+    { name: "Error", message: "Static node does not allow timeout" },
+  );
+});
+
+test("test static node validation fork", async () => {
+  const graph_data = anonymization({
+    nodes: {
+      static1: {
+        fork: 1,
+      },
+    },
+  });
+  await assert.rejects(
+    async () => {
+      await graphDataTestRunner(__filename, graph_data, { echoAgent });
+    },
+    { name: "Error", message: "Static node does not allow fork" },
+  );
+});
+
+
