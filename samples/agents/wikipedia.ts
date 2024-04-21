21364d5930a0f0d12bab598ca84c3e59daad6db5
snakajima <satoshi.nakajima@gmail.com>
2024-04-21 05:42:38 -0700

    summary

--- samples/agents/wikipedia.ts
+++ samples/agents/wikipedia.ts
@@ -1,8 +1,8 @@
 import { AgentFunction } from "@/graphai";
 import wiki from "wikipedia";
 
-export const wikipediaAgent: AgentFunction<{ inputKey: string; lang?: string }, Record<string, any> | undefined> = async ({ inputs, params }) => {
-  const { inputKey, lang } = params;
+export const wikipediaAgent: AgentFunction<{ inputKey: string; lang?: string, summary?: boolean }, Record<string, any> | undefined> = async ({ inputs, params }) => {
+  const { inputKey, lang, summary } = params;
   const query = inputs[0][inputKey];
   try {
     if (lang) {
@@ -12,7 +12,7 @@ export const wikipediaAgent: AgentFunction<{ inputKey: string; lang?: string }, 
     const search_res = search.results[0];
 
     const page = await wiki.page(search_res["title"]);
-    const content = await page.content();
+    const content = await (summary ? page.summary() : page.content());
     return { content, ...search.results[0] };
   } catch (error) {
     console.log(error);
