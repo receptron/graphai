3d850242ef13d417df0d5319ce58ae8bf049032d
isamu <isamu@to-kyo.to>
2024-04-21 11:35:08 +0900

    add loop and nested agent sample

--- tests/graphai/test_loop_2.ts
+++ tests/graphai/test_loop_2.ts
@@ -1,9 +1,11 @@
+import { AgentFunction } from "@/graphai";
 import { graphDataTestRunner } from "~/utils/runner";
+import { defaultTestAgents } from "~/agents/agents";
 
 import test from "node:test";
 import assert from "node:assert";
 
-const graphdata_push = {
+const graphdata_counter = {
   loop: {
     count: 10,
   },
@@ -19,11 +21,44 @@ const graphdata_push = {
   },
 };
 
+const counterAgent: AgentFunction = async ({ inputs }) => {
+  console.log(inputs);
+  return { v: (inputs[0].v || 0) + 1 };
+};
+
 test("test counter", async () => {
-  const result = await graphDataTestRunner(__filename, graphdata_push, {
-    counterAgent: async ({ inputs }) => {
-      return { v: inputs[0].v + 1 };
-    },
+  const result = await graphDataTestRunner(__filename, graphdata_counter, {
+    counterAgent,
   });
   assert.deepStrictEqual(result, { data: { v: 9 }, counter: { v: 10 } });
+});
+
+test("test counter2", async () => {
+  const nested_graphdata = {
+    loop: {
+      count: 10,
+    },
+    nodes: {
+      workingMemory: {
+        value: {},
+        update: "nested1", // update data from nested1 data
+      },
+      nested1: {
+        agentId: "nestedAgent",
+        params: {
+          nodeId: "counter", // nestedAgent result is counter node result in graphdata_counter
+          inputNodes: ["data"], // inject workingMemory data to data node in graphdata_counter
+          graph: graphdata_counter,
+        },
+        inputs: ["workingMemory"],
+      },
+    },
+  };
+
+  const result = await graphDataTestRunner(__filename, nested_graphdata, {
+    ...defaultTestAgents,
+    counterAgent,
+  });
+  console.log(result);
+  assert.deepStrictEqual(result, { workingMemory: { v: 90 }, nested1: { v: 100 } });
 });
