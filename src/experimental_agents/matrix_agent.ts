8965d0195e2afdb4f80569c7c0cfae2a686ac7c2
snakajima <satoshi.nakajima@gmail.com>
2024-04-22 13:14:59 -0700

    comments

--- src/experimental_agents/matrix_agent.ts
+++ src/experimental_agents/matrix_agent.ts
@@ -1,5 +1,12 @@
 import { AgentFunction } from "@/graphai";
 
+// This agent calculates the dot product of an array of vectors and a vector,
+// typically used to calculate cosine similarity of embedding vectors.
+// Parameters:
+//  inputKey: string; // specifies the property to retrieve from inputs. The default is "contents"
+// Inputs:
+//  inputs[0].inputKey: Two dimentional array of numbers.
+//  inputs[1].inputKey: Two dimentional array of numbers (but the array size is 1 for the first dimention)
 export const dotProductAgent: AgentFunction<
   {
     inputKey?: string;
