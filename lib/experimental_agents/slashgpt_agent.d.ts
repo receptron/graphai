bade6779733fce9a138aab7b0e6f3b98b9454dd3
snakajima <satoshi.nakajima@gmail.com>
2024-04-22 16:23:29 -0700

    built

--- lib/experimental_agents/slashgpt_agent.d.ts
+++ lib/experimental_agents/slashgpt_agent.d.ts
@@ -4,6 +4,7 @@ export declare const slashGPTAgent: AgentFunction<{
     manifest: ManifestData;
     query?: string;
     function_result?: boolean;
+    inputKey?: string;
 }, {
     content: string;
 }>;
--- lib/experimental_agents/string_agent.d.ts
+++ lib/experimental_agents/string_agent.d.ts
@@ -1,4 +1,5 @@
 import { AgentFunction } from "../graphai";
+export declare const getStringInput: (input: any, inputKey: string) => any;
 export declare const stringTemplateAgent: AgentFunction<{
     template: string;
     inputKey?: string;
