1e6febc1b1424f9e82bd8c39d25809eb7ee8c947
snakajima <satoshi.nakajima@gmail.com>
2024-04-22 20:29:58 -0700

    no inputKey for stringTemplate agent

--- tests/agents/test_string_agent.ts
+++ tests/agents/test_string_agent.ts
@@ -8,28 +8,6 @@ test("test stringTemplateAgent simple", async () => {
   const result = await stringTemplateAgent({
     ...defaultTestContext,
     params: { template: "${0}: ${1}" },
-    inputs: [{ content: "hello" }, { content: "test" }],
-  });
-  assert.deepStrictEqual(result, {
-    content: "hello: test",
-  });
-});
-
-test("test stringTemplateAgent simple", async () => {
-  const result = await stringTemplateAgent({
-    ...defaultTestContext,
-    params: { template: "${0}: ${1}", inputKey: "key" },
-    inputs: [{ key: "hello" }, { key: "test" }],
-  });
-  assert.deepStrictEqual(result, {
-    content: "hello: test",
-  });
-});
-
-test("test stringTemplateAgent simple 3", async () => {
-  const result = await stringTemplateAgent({
-    ...defaultTestContext,
-    params: { template: "${0}: ${1}", inputKey: "key" },
     inputs: ["hello", "test"],
   });
   assert.deepStrictEqual(result, {
