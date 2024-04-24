b096d743d965e1c13e339ada7ca00444e745741a
snakajima <satoshi.nakajima@gmail.com>
2024-04-24 15:23:13 -0700

    forkAgent

---
+++ src/experimental_agents/fork_agent.ts
@@ -0,0 +1,43 @@
+import { GraphAI, GraphData, AgentFunction } from "@/graphai";
+import { assert } from "@/utils/utils";
+
+export const forkAgent: AgentFunction<
+  {
+    graph: GraphData;
+    resultFrom: string;
+    injectionTo?: string;
+  },
+  {
+    contents: Array<any>;
+  },
+  any
+> = async ({ params, inputs, agents, log, taskManager }) => {
+  if (taskManager) {
+    const status = taskManager.getStatus();
+    assert(status.concurrency > status.running, `forkAgent: Concurrency is too low: ${status.concurrency}`);
+  }
+
+  const input = inputs[0];
+  const graphs: Array<GraphAI> = input.map((data: any) => {
+    const graph = new GraphAI(params.graph, agents || {}, taskManager);
+    if (params.injectionTo) {
+      graph.injectValue(params.injectionTo, data);
+    }
+    return graph;
+  });
+
+  const promises = graphs.map((graph) => {
+    return graph.run();
+  });
+  const results = await Promise.all(promises);
+  const contents = results.map((result) => {
+    return result[params.resultFrom];
+  });
+  if (log) {
+    const logs = graphs.map((graph) => {
+      return graph.transactionLogs();
+    });
+    log.push(...logs.flat());
+  }
+  return { contents };
+};
