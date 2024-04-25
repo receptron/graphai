36ba75143f8d3af2642f361fbd31e19a7ca459a3
isamu <isamu@to-kyo.to>
2024-04-25 17:43:52 +0900

    update fork test

--- tests/graphai/test_fork.ts
+++ tests/graphai/test_fork.ts
@@ -1,13 +1,12 @@
 import { AgentFunction } from "@/graphai";
 import { graphDataTestRunner } from "~/utils/runner";
-import { sleeperAgent } from "@/experimental_agents";
+import { sleeperAgent, mapAgent } from "@/experimental_agents";
 
 import test from "node:test";
 import assert from "node:assert";
 
 const testAgent1: AgentFunction = async ({ debugInfo: { nodeId }, inputs }) => {
   // console.log("executing", nodeId, params, inputs);
-
   const result = {
     [nodeId]: [nodeId, inputs.map((a) => Object.values(a).flat())]
       .flat()
@@ -18,45 +17,136 @@ const testAgent1: AgentFunction = async ({ debugInfo: { nodeId }, inputs }) => {
   return result;
 };
 
+const testAgent1a: AgentFunction = async ({ debugInfo: { nodeId }, inputs }) => {
+  // console.log("executing", nodeId, inputs);
+  const result = {
+    [nodeId]: [
+      nodeId,
+      inputs.map((a) => {
+        return Object.values(a);
+      }),
+    ]
+      .flat()
+      .filter((a) => !!a)
+      .join(":"),
+  };
+  // console.log("completing", nodeId, result);
+  return result;
+};
+
+const copy2ArrayAgent: AgentFunction = async ({ inputs, params }) => {
+  return new Array(10).fill(undefined).map((_, i) => {
+    return inputs[0];
+  });
+};
+
 test("test fork 1", async () => {
   const forkGraph = {
     nodes: {
       node1: {
-        agentId: "testAgent1",
-        params: {},
+        agentId: "testAgent1a",
       },
       node2: {
-        agentId: "testAgent1",
-        params: {},
-        fork: 10,
+        agentId: "copy2ArrayAgent",
         inputs: ["node1"],
       },
-      node3: {
-        agentId: "testAgent1",
-        params: {},
-        // fork: 10,
+      nestedNode: {
+        agentId: "mapAgent",
         inputs: ["node2"],
+        params: {
+          injectionTo: "buffer",
+          resultFrom: "node3",
+        },
+        graph: {
+          nodes: {
+            buffer: {
+              value: {},
+            },
+            node2: {
+              agentId: "testAgent1a",
+              inputs: ["buffer"],
+            },
+            node3: {
+              agentId: "testAgent1a",
+              inputs: ["node2"],
+            },
+          },
+        },
       },
     },
   };
 
-  const result = await graphDataTestRunner(__filename, forkGraph, { testAgent1 });
-  // console.log(result);
+  const result = await graphDataTestRunner(__filename, forkGraph, { testAgent1a, mapAgent, copy2ArrayAgent });
+  // console.log(JSON.stringify(result, null, "  "));
   assert.deepStrictEqual(result, {
-    node1: { node1: "node1" },
-    node2_0: { node2_0: "node2_0:node1" },
-    node2_1: { node2_1: "node2_1:node1" },
-    node2_2: { node2_2: "node2_2:node1" },
-    node2_3: { node2_3: "node2_3:node1" },
-    node2_4: { node2_4: "node2_4:node1" },
-    node2_5: { node2_5: "node2_5:node1" },
-    node2_6: { node2_6: "node2_6:node1" },
-    node2_7: { node2_7: "node2_7:node1" },
-    node2_8: { node2_8: "node2_8:node1" },
-    node2_9: { node2_9: "node2_9:node1" },
-    node3: {
-      node3:
-        "node3:node2_0:node1:node2_1:node1:node2_2:node1:node2_3:node1:node2_4:node1:node2_5:node1:node2_6:node1:node2_7:node1:node2_8:node1:node2_9:node1",
+    node1: {
+      node1: "node1",
+    },
+    node2: [
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+      {
+        node1: "node1",
+      },
+    ],
+    nestedNode: {
+      contents: [
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+        {
+          node3: "node3:node2:node1",
+        },
+      ],
     },
   });
 });
