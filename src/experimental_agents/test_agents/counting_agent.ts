2ab21175cdc47055ce1d6ac8c9dfcda4f37f683d
isamu <isamu@to-kyo.to>
2024-04-26 18:23:31 +0900

    format

--- src/experimental_agents/test_agents/counting_agent.ts
+++ src/experimental_agents/test_agents/counting_agent.ts
@@ -3,7 +3,7 @@ import { AgentFunction } from "@/graphai";
 export const countingAgent: AgentFunction = async ({ params }) => {
   return {
     list: new Array(params.count).fill(undefined).map((_, i) => {
-      return i
+      return i;
     }),
   };
 };
