25f1c4078b5e9246e30b7ace76d2c895b6bc2ae8
isamu <isamu@to-kyo.to>
2024-04-26 17:45:02 +0900

    fix eslint

--- src/experimental_agents/test_agents/copy_message_agent.ts
+++ src/experimental_agents/test_agents/copy_message_agent.ts
@@ -2,7 +2,7 @@ import { AgentFunction } from "@/graphai";
 
 export const copyMessageAgent: AgentFunction = async ({ params }) => {
   return {
-    messages: new Array(params.count).fill(undefined).map((_, i) => {
+    messages: new Array(params.count).fill(undefined).map(() => {
       return params.message;
     }),
   };
