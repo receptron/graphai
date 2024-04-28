c5f7f2f317db56e7ecb1b3267eb78de33287d781
isamu <isamu@to-kyo.to>
2024-04-28 13:44:12 +0900

    add packages

---
+++ src/experimental_agents/data_agents/packages.ts
@@ -0,0 +1,9 @@
+import totalAgent from "@/experimental_agents/data_agents/total_agent";
+import dataObjectMergeTemplateAgent from "@/experimental_agents/data_agents/data_object_merge_template_agent";
+import dataSumTemplateAgent from "@/experimental_agents/data_agents/data_sum_template_agent";
+
+export {
+  totalAgent,
+  dataObjectMergeTemplateAgent,
+  dataSumTemplateAgent,
+};
---
+++ src/experimental_agents/string_agents/packages.ts
@@ -0,0 +1,7 @@
+import stringSplitterAgentInfo from "@/experimental_agents/string_agents/string_splitter_agent";
+import stringTemplateAgentInfo from "@/experimental_agents/string_agents/string_template_agent";
+
+export {
+  stringSplitterAgentInfo,
+  stringTemplateAgentInfo,
+};
