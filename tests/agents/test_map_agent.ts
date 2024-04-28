274e269ce3966792188b5db144204ee928ad4477
snakajima <satoshi.nakajima@gmail.com>
2024-04-28 06:19:09 -0700

    You like banana

--- tests/agents/test_map_agent.ts
+++ tests/agents/test_map_agent.ts
@@ -49,3 +49,26 @@ test("test map_agent 2", async () => {
     node2: [{ content: "I love apple." }, { content: "I love orange." }, { content: "I love banana." }, { content: "I love lemon." }],
   });
 });
+
+test("test map_agent 3", async () => {
+  const result = await mapAgent({
+    ...defaultTestContext,
+    agents: { mapAgent, stringTemplateAgent },
+    graphData: {
+      nodes: {
+        node2: {
+          agentId: "stringTemplateAgent",
+          params: {
+            template: "${1} ${2} ${0}.",
+          },
+          inputs: ["$0", "$1", "$2"],
+          isResult: true,
+        },
+      },
+    },
+    inputs: [["apple", "orange", "banana", "lemon"], "You", "like"],
+  });
+  assert.deepStrictEqual(result, {
+    node2: [{ content: "You like apple." }, { content: "You like orange." }, { content: "You like banana." }, { content: "You like lemon." }],
+  });
+});
