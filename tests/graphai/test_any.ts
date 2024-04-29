a3316cf659fedc6f45bd0301c37a2f519a1eee3e
snakajima <satoshi.nakajima@gmail.com>
2024-04-29 05:32:27 -0700

    negative case

--- tests/graphai/test_any.ts
+++ tests/graphai/test_any.ts
@@ -1,38 +1,61 @@
 import { graphDataTestRunner } from "~/utils/runner";
 import { defaultTestAgents } from "@/utils/test_agents";
-import { fileBaseName } from "~/utils/file_utils";
+import { GraphAI } from "@/graphai";
 
 import test from "node:test";
 import assert from "node:assert";
 
-const graphdata_push = {
+const graphdata_any = {
   nodes: {
     source: {
-      value: { yes: true, no: false, message: { apple: "red" } },
+      value: {},
     },
     positive: {
       agentId: "sleeperAgent",
       anyInput: true,
       isResult: true,
-      inputs: ["source.yes", "source.message"]
+      inputs: ["source.yes"],
     },
     negative: {
       agentId: "sleeperAgent",
       anyInput: true,
       isResult: true,
-      inputs: ["source.no", "source.message"]
+      inputs: ["source.no"],
     },
   },
 };
 
-test("test loop & push", async () => {
-  const result = await graphDataTestRunner(__filename, graphdata_push, defaultTestAgents, () => {}, false);
-  console.log(result);
-  /*
+test("test any 1", async () => {
+  const result = await graphDataTestRunner(__filename, graphdata_any, defaultTestAgents, () => {}, false);
+  assert.deepStrictEqual(result, {});
+});
+
+test("test any yes", async () => {
+  const result = await graphDataTestRunner(
+    __filename,
+    graphdata_any,
+    defaultTestAgents,
+    (graph: GraphAI) => {
+      graph.injectValue("source", { yes: { apple: "red" } });
+    },
+    false,
+  );
   assert.deepStrictEqual(result, {
-    // array: ["hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello"],
-    // item: "hello",
-    reducer: ["hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello", "hello"],
+    positive: { apple: "red" },
   });
-  */
+});
+
+test("test any no", async () => {
+  const result = await graphDataTestRunner(
+    __filename,
+    graphdata_any,
+    defaultTestAgents,
+    (graph: GraphAI) => {
+      graph.injectValue("source", { no: { lemon: "yellow" } });
+    },
+    false,
+  );
+  assert.deepStrictEqual(result, {
+    negative: { lemon: "yellow" },
+  });
 });
