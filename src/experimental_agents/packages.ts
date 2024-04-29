de53993889880bbd2a992b8253a7becd548f77e8
isamu <isamu@to-kyo.to>
2024-04-29 10:36:27 +0900

    agent list

--- src/experimental_agents/packages.ts
+++ src/experimental_agents/packages.ts
@@ -3,9 +3,8 @@ export * from "./sleeper_agents/packages";
 export * from "./data_agents/packages";
 export * from "./array_agents/packages";
 export * from "./matrix_agents/packages";
-
-export * from "./graph_agents/packages";
 export * from "./test_agents/packages";
+export * from "./graph_agents/packages";
 
 import slashGPTAgent from "./slashgpt_agent";
 import stringEmbeddingsAgent from "./embedding_agent";
