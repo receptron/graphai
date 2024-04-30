a94cdba25353a1e36debfa46dd388097bca0ae54
snakajima <satoshi.nakajima@gmail.com>
2024-04-30 06:51:28 -0700

    exclude

--- src/experimental_agents/data_agents/property_filter_agent.ts
+++ src/experimental_agents/data_agents/property_filter_agent.ts
@@ -1,14 +1,20 @@
 import { AgentFunction } from "@/graphai";
 import deepmerge from "deepmerge";
 
-export const propertyFilterAgent: AgentFunction<{ includes: Array<string> }> = async ({ inputs, params }) => {
+export const propertyFilterAgent: AgentFunction<{ include?: Array<string>; exclude?: Array<string> }> = async ({ inputs, params }) => {
   const [input] = inputs;
-  const { includes } = params;
-  return includes.reduce((tmp: Record<string, any>, propId) => {
-    tmp[propId] = input[propId];
+  const { include, exclude } = params;
+  const propIds = include ? include : Object.keys(input);
+  const excludeSet = new Set(exclude ?? []);
+  return propIds.reduce((tmp: Record<string, any>, propId) => {
+    if (!excludeSet.has(propId)) {
+      tmp[propId] = input[propId];
+    }
     return tmp;
   }, {});
 };
+
+const inputs = [{ color: "red", model: "Model 3", type: "EV", maker: "Tesla", range: 300 }];
 
 const propertyFilterAgentInfo = {
   name: "propertyFilterAgent",
@@ -16,12 +22,17 @@ const propertyFilterAgentInfo = {
   mock: propertyFilterAgent,
   samples: [
     {
-      inputs: [{ color: "red", size: 1, type: "car" }],
-      params: { includes: ["color", "size"] },
-      result: { color: "red", size: 1 },
+      inputs,
+      params: { include: ["color", "model"] },
+      result: { color: "red", model: "Model 3" },
+    },
+    {
+      inputs,
+      params: { exclude: ["color", "model"] },
+      result: { type: "EV", maker: "Tesla", range: 300 },
     },
   ],
-  description: "Filter properties",
+  description: "Filter properties based on property name either with 'include' or 'exclude'",
   category: ["data"],
   author: "Receptron team",
   repository: "https://github.com/receptron/graphai",
