f6b096000e804105aa42818523bb26890c8e31b6
isamu <isamu@to-kyo.to>
2024-04-30 12:31:46 +0900

    add tests

--- tests/graphai/test_bypass.ts
+++ tests/graphai/test_bypass.ts
@@ -189,3 +189,48 @@ test("test bypass4", async () => {
 
   // console.log("COMPLETE 1");
 });
+
+///
+
+test("test bypass 5", async () => {
+  const graph_data = {
+    nodes: {
+      echo: {
+        agentId: "echoAgent",
+        params: {
+          message: "hello",
+        },
+      },
+      bypassAgent: {
+        agentId: "bypassAgent",
+        inputs: ["echo", "echo", "echo"],
+      },
+      bypassAgent2: {
+        agentId: "bypassAgent",
+        inputs: ["bypassAgent", "bypassAgent"],
+      },
+      bypassAgent3: {
+        agentId: "bypassAgent",
+        inputs: ["bypassAgent2", "bypassAgent2"],
+      },
+    },
+  };
+  const result = await graphDataTestRunner(__filename, graph_data, defaultTestAgents);
+  console.log(JSON.stringify(result));
+  assert.deepStrictEqual(result, {
+    bypassAgent2: [
+      {
+        message: "hello",
+      },
+    ],
+    bypassAgent: [
+      {
+        message: "hello",
+      },
+    ],
+    echo: {
+      message: "hello",
+    },
+  });
+  // console.log("COMPLETE 1");
+});
--- tests/graphai/test_map.ts
+++ tests/graphai/test_map.ts
@@ -91,3 +91,103 @@ test("test map 2", async () => {
     { content: "I love lemon." },
   ]);
 });
+
+// nest graph and flat
+const graphdata_map3 = {
+  nodes: {
+    source1: {
+      value: ["hello", "hello2"],
+    },
+    nestedNode: {
+      agentId: "mapAgent",
+      inputs: ["source1"],
+      graph: {
+        nodes: {
+          node1: {
+            agentId: "bypassAgent",
+            inputs: ["$0"],
+            isResult: true,
+          },
+        },
+      },
+    },
+    result: {
+      agentId: "bypassAgent",
+      inputs: ["nestedNode.node1.$0"],
+    },
+  },
+};
+
+test("test map 3", async () => {
+  const result = await graphDataTestRunner("test_map3", graphdata_map3, defaultTestAgents);
+
+  assert.deepStrictEqual(result.result, [[["hello"], ["hello2"]]]);
+});
+
+const graphdata_map4 = {
+  nodes: {
+    source1: {
+      value: ["hello", "hello2"],
+    },
+    nestedNode: {
+      agentId: "mapAgent",
+      inputs: ["source1"],
+      graph: {
+        nodes: {
+          node1: {
+            agentId: "bypassAgent",
+            inputs: ["$0"],
+            isResult: true,
+          },
+        },
+      },
+    },
+    result: {
+      agentId: "bypassAgent",
+      params: {
+        flat: 1,
+      },
+      inputs: ["nestedNode.node1"],
+    },
+  },
+};
+
+test("test map 4", async () => {
+  const result = await graphDataTestRunner("test_map4", graphdata_map4, defaultTestAgents);
+
+  assert.deepStrictEqual(result.result, [["hello"], ["hello2"]]);
+});
+
+const graphdata_map5 = {
+  nodes: {
+    source1: {
+      value: ["hello", "hello2"],
+    },
+    nestedNode: {
+      agentId: "mapAgent",
+      inputs: ["source1"],
+      graph: {
+        nodes: {
+          node1: {
+            agentId: "bypassAgent",
+            inputs: ["$0"],
+            isResult: true,
+          },
+        },
+      },
+    },
+    result: {
+      agentId: "bypassAgent",
+      params: {
+        flat: 2,
+      },
+      inputs: ["nestedNode.node1"],
+    },
+  },
+};
+
+test("test map 5", async () => {
+  const result = await graphDataTestRunner("test_map5", graphdata_map5, defaultTestAgents);
+
+  assert.deepStrictEqual(result.result, ["hello", "hello2"]);
+});
