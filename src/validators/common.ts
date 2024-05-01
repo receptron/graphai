b7eb132a556d6e97077cfc7e146516c857a03c09
snakajima <satoshi.nakajima@gmail.com>
2024-04-30 20:55:36 -0700

    version 0.2

--- src/validators/common.ts
+++ src/validators/common.ts
@@ -1,4 +1,4 @@
-export const graphDataAttributeKeys = ["nodes", "concurrency", "agentId", "loop", "verbose"];
+export const graphDataAttributeKeys = ["nodes", "concurrency", "agentId", "loop", "verbose", "version"];
 
 export const computedNodeAttributeKeys = ["inputs", "anyInput", "params", "retry", "timeout", "agentId", "graph", "isResult"];
 export const staticNodeAttributeKeys = ["value", "update", "isResult"];
