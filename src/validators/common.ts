4e82f75d5a5b6e4489b306da0fff3bae875dc47e
snakajima <satoshi.nakajima@gmail.com>
2024-05-01 21:21:14 -0700

    task priority

--- src/validators/common.ts
+++ src/validators/common.ts
@@ -1,4 +1,4 @@
 export const graphDataAttributeKeys = ["nodes", "concurrency", "agentId", "loop", "verbose", "version"];
 
-export const computedNodeAttributeKeys = ["inputs", "anyInput", "params", "retry", "timeout", "agentId", "graph", "isResult"];
+export const computedNodeAttributeKeys = ["inputs", "anyInput", "params", "retry", "timeout", "agentId", "graph", "isResult", "priority"];
 export const staticNodeAttributeKeys = ["value", "update", "isResult"];
