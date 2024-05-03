aedafc3ec0bffddd2047efb6bfeb647625becb1e
snakajima <satoshi.nakajima@gmail.com>
2024-05-03 10:08:09 -0700

    service_agents

--- samples/sample_wikipedia.ts
+++ samples/sample_wikipedia.ts
@@ -2,9 +2,10 @@ import "dotenv/config";
 
 import { graphDataTestRunner } from "~/utils/runner";
 import { interactiveInputTextAgent } from "./agents/interactiveInputAgent";
-import { wikipediaAgent } from "./agents/wikipedia";
+import { wikipediaAgent } from "@/experimental_agents";
 
 const graph_data = {
+  version: 0.2,
   nodes: {
     interactiveInputAgent: {
       agentId: "interactiveInputTextAgent",
