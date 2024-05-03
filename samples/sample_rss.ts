3d301833663eb32674e4adb939de56a75f759a09
snakajima <satoshi.nakajima@gmail.com>
2024-05-03 12:35:08 -0700

    translated

--- samples/sample_rss.ts
+++ samples/sample_rss.ts
@@ -2,7 +2,9 @@ import "dotenv/config";
 
 import { graphDataTestRunner } from "~/utils/runner";
 import {
-  rssAgent, propertyFilterAgent,
+  rssAgent, 
+  propertyFilterAgent,
+  gloqAgent,
 } from "@/experimental_agents";
 
 const graph_data = {
@@ -19,9 +21,18 @@ const graph_data = {
       agentId: "propertyFilterAgent",
       isResult: true,
       params: {
-        include: ["title"] // , "link", "content"],
+        include: ["title", "link", "content"],
       },
-      inputs: ["rssFeed.feed.entry.$0"]
+      inputs: ["rssFeed.feed.entry.$1"]
+    },
+    query: {
+      agentId: "gloqAgent",
+      params: {
+        model: "Llama3-70b-8192", // "mixtral-8x7b-32768",
+        query: "次のHTMLからテキストだけを抜き出し、省略せずに、全文を日本語に翻訳して。余計なことは言わずに、翻訳した文章だけ答えて。",
+      },
+      isResult: true,
+      inputs: ["filter.content._"],
     }
   },
 };
@@ -30,9 +41,11 @@ const main = async () => {
   const result = await graphDataTestRunner("sample_wiki.log", graph_data, {
     rssAgent,
     propertyFilterAgent,
+    gloqAgent
   }) as any;
   // console.log(result.rssFeed.feed.entry[0]);
   console.log(result.filter);
+  console.log(result.query.choices);
 };
 if (process.argv[1] === __filename) {
   main();
