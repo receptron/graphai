1d5580ebc73ae6e55e6ca22125b495c7d50dd25a
snakajima <satoshi.nakajima@gmail.com>
2024-05-06 11:13:53 -0700

    loop

--- samples/sample_benchmark.ts
+++ samples/sample_benchmark.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { graphDataTestRunner } from "~/utils/runner";
-import { sleeperAgent, groqAgent, fetchAgent, copyAgent, nestedAgent } from "@/experimental_agents";
+import { groqAgent, fetchAgent, shiftAgent, nestedAgent } from "@/experimental_agents";
 
 const graph_data = {
   version: 0.2,
@@ -25,16 +25,40 @@ const graph_data = {
       agent: (rows: Array<Record<string, any>>) => rows.map((row) => row.row),
       inputs: ["fetch.rows"],
     },
-    groq: {
-      agent: "groqAgent",
-      params: {
-        model: "Llama3-8b-8192",
+    loop: {
+      agent: "nestedAgent",
+      inputs: ["rows"],
+      graph: {
+        version: 0.2,
+        loop: {
+          while: '$0',
+        },
+        nodes: {
+          $0: {
+            value: undefined,
+            update: "retriever.array",
+          },
+          retriever: {
+            agent: "shiftAgent",
+            inputs: ["$0"],
+          },
+          outputQA: {
+            agent: (item: Record<string, string>) => console.log(`Q: ${item.question}\nA0: ${item.answer}`),
+            inputs: ["retriever.item"],
+          },
+          groq: {
+            agent: "groqAgent",
+            params: {
+              model: "Llama3-8b-8192",
+            },
+            inputs: ["retriever.item.question"],
+          },
+          output: {
+            agent: (answer: string) => console.log(`A: ${answer}\n`),
+            inputs: ["groq.choices.$0.message.content"],
+          },
+        }
       },
-      inputs: ["rows.$0.question"],
-    },
-    output: {
-      agent: (answer: string) => console.log(answer),
-      inputs: ["groq.choices.$0.message.content"],
     },
   },
 };
@@ -45,9 +69,8 @@ export const main = async () => {
     graph_data,
     {
       groqAgent,
-      sleeperAgent,
+      shiftAgent,
       fetchAgent,
-      copyAgent,
       nestedAgent,
     },
     () => {},
