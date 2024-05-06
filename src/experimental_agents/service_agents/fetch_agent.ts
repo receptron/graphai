2bf47eebc0579d7cf22388ca97bbe1fe966b9168
snakajima <satoshi.nakajima@gmail.com>
2024-05-06 07:37:18 -0700

    fetchAgent unit test

--- src/experimental_agents/service_agents/fetch_agent.ts
+++ src/experimental_agents/service_agents/fetch_agent.ts
@@ -1,8 +1,37 @@
 import { AgentFunction } from "@/graphai";
 
-export const fetchAgent: AgentFunction<undefined, any, string> = async ({ inputs }) => {
-  const [ url ] = inputs;
-  const response = await fetch(url);
+export const fetchAgent: AgentFunction<{ debug?: boolean }, any, any> = async ({ inputs, params }) => {
+  const [ baseUrl, queryParams, baseHeaders, body ] = inputs;
+
+  const url = new URL(baseUrl);
+  const headers = baseHeaders ? { ...baseHeaders } : {};
+
+  if (queryParams) {
+    const params = new URLSearchParams(queryParams);
+    url.search = params.toString();
+  }
+
+  if (body) {
+    headers['Content-Type'] = 'application/json';
+  }
+
+  const fetchOptions: RequestInit = {
+    method: body ? "POST" : "GET",
+    headers: new Headers(headers),
+    body: body ? JSON.stringify(body) : undefined,
+  };
+
+  if (params?.debug) {
+    return {
+      url: url.toString(),
+      method: fetchOptions.method,
+      headers,
+      body: fetchOptions.body
+    }
+  }
+
+  const response = await fetch(url.toString(), fetchOptions);
+
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
@@ -13,6 +42,20 @@ const fetchAgentInfo = {
   name: "fetchAgent",
   agent: fetchAgent,
   mock: fetchAgent,
+  samples: [
+    {
+      inputs: ["https://www.google.com"],
+      params: {
+        debug: true,
+      },
+      result: {
+        body: undefined,
+        headers: {},
+        method: 'GET',
+        url: 'https://www.google.com/'
+      },
+    },
+  ],
   description: "Retrieves JSON data from the specified URL",
   category: ["data"],
   author: "Receptron",
