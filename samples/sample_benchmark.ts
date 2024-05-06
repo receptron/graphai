a2fe9dbcead86f8e444ec4842026283ff670e2c8
snakajima <satoshi.nakajima@gmail.com>
2024-05-06 13:59:36 -0700

    comments

--- samples/sample_benchmark.ts
+++ samples/sample_benchmark.ts
@@ -6,6 +6,7 @@ const graph_data = {
   version: 0.2,
   nodes: {
     GSM8: {
+      // This node specifies the URL and query paramters to fetch GSM8K dataset.
       value: {
         url: "https://datasets-server.huggingface.co/rows",
         query: {
@@ -18,11 +19,13 @@ const graph_data = {
       },
     },
     fetch: {
+      // This node fetches the dataset over HTTP.
       agent: "fetchAgent",
       inputs: ["GSM8.url", "GSM8.query"],
     },
     rows: {
-      agent: (rows: Array<Record<string, any>>) => rows.map((row) => row.row),
+      // This node extract the "row" property from each item in the dataset.
+      agent: (items: Array<Record<string, any>>) => items.map((item) => item.row),
       inputs: ["fetch.rows"],
     },
     debugOutputRow: {
@@ -30,24 +33,30 @@ const graph_data = {
       inputs: ["rows.$0"],
     },
     loop: {
+      // This node interate all the items in the dataset using the nested graph
       agent: "nestedAgent",
       inputs: ["rows"],
       isResult: true,
       graph: {
+        // This graph continues until the array on node "$0" becomes empty
         version: 0.2,
         loop: {
           while: "$0",
         },
         nodes: {
+          // This node receives the inputs[0] (data from "rows" node on outer graph) initially
+          // then, updated with the data from "retriever" node after each iteration.
           $0: {
             value: undefined,
             update: "retriever.array",
           },
+          // This node accumurate asnwers for each question in the dataset.
           answers: {
             value: [],
             update: "reducer",
             isResult: true,
           },
+          // This node takes the first item from the array from node "$0".
           retriever: {
             agent: "shiftAgent",
             inputs: ["$0"],
@@ -57,6 +66,7 @@ const graph_data = {
             inputs: ["retriever.item"],
           },
           groq: {
+            // This node sends the question on the current item to Llama3 on groq and get the answer.
             agent: "groqAgent",
             params: {
               model: "Llama3-8b-8192",
@@ -64,6 +74,7 @@ const graph_data = {
             inputs: ["retriever.item.question"],
           },
           reducer: {
+            // This node pushs the answer from Llama3 into the answer array.
             agent: "pushAgent",
             inputs: ["answers", "groq.choices.$0.message.content"],
           },
