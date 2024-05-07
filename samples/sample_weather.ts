5a780bfde76c1918dd9da8c767fa9749b18e23d2
snakajima <satoshi.nakajima@gmail.com>
2024-05-07 15:01:05 -0700

    system message

---
+++ samples/sample_weather.ts
@@ -0,0 +1,74 @@
+import "dotenv/config";
+import { graphDataTestRunner } from "~/utils/runner";
+import { groqAgent, shiftAgent, nestedAgent } from "@/experimental_agents";
+import input from "@inquirer/input";
+
+const graph_data = {
+  version: 0.2,
+  loop: {
+    while: "continue",
+  },
+  nodes: {
+    continue: {
+      value: true,
+      update: "checkInput",
+    },
+    messages: {
+      // This node holds the conversation, array of messages.
+      value: [ {role: "system", content:"You are a meteorologist, and will give weather focast to the user using weather API."} ],
+      update: "reducer",
+      isResult: true,
+    },
+    userInput: {
+      // This node receives an input from the user.
+      agent: () => input({ message: "You:" }),
+    },
+    checkInput: {
+      agent: (query: string) => query !== "/bye",
+      inputs: ["userInput"],
+    },
+    appendedMessages: {
+      // This node appends the user's input to the array of messages.
+      agent: (content: string, messages: Array<any>) => [...messages, { role: "user", content }],
+      inputs: ["userInput", "messages"],
+      if: "checkInput",
+    },
+    groq: {
+      // This node sends those messages to Llama3 on groq to get the answer.
+      agent: "groqAgent",
+      params: {
+        model: "Llama3-8b-8192",
+      },
+      inputs: [undefined, "appendedMessages"],
+    },
+    output: {
+      // This node displays the responce to the user.
+      agent: (answer: string) => console.log(`Llama3: ${answer}\n`),
+      inputs: ["groq.choices.$0.message.content"],
+    },
+    reducer: {
+      // This node append the responce to the messages.
+      agent: "pushAgent",
+      inputs: ["appendedMessages", "groq.choices.$0.message"],
+    },
+  },
+};
+
+export const main = async () => {
+  const result = await graphDataTestRunner(
+    __filename,
+    graph_data,
+    {
+      groqAgent,
+      shiftAgent,
+      nestedAgent,
+    },
+    () => {},
+    false,
+  );
+  console.log("Complete", result);
+};
+
+if (process.argv[1] === __filename) {
+  main();
+}
