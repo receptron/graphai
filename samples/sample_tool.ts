a92c80a0e6eb55a9b9f84a9052821442ba937487
snakajima <satoshi.nakajima@gmail.com>
2024-05-07 06:00:56 -0700

    clean up

--- samples/sample_tool.ts
+++ samples/sample_tool.ts
@@ -26,30 +26,11 @@ const tools = [{
   },
 }];
 
-const tools_baseball = [{
-  "type": "function",
-  "function": {
-    "name": "get_game_score",
-    "description": "Get the score for a given NBA game",
-    "parameters": {
-      "type": "object",
-      "properties": {
-        "team_name": {
-          "type": "string",
-          "description": "The name of the NBA team (e.g. 'Golden State Warriors')",
-        }
-      },
-      "required": ["team_name"],
-    },
-  },
-}]
-
 const graph_data = {
   version: 0.2,
   nodes: {
     question: {
       value: ["apple", "eggplant"],
-      // value: ["What was the score of the Warriors game?"],
     },
     groq: {
       // This node sends those messages to Llama3 on groq to get the answer.
@@ -57,9 +38,8 @@ const graph_data = {
       params: {
         model: "Llama3-8b-8192", // "llama3-70b-8192", // "Llama3-8b-8192",
         system: "You are a function calling LLM that categorize the specified food by calling categorize function.",
-        // system: "You are a function calling LLM that uses the data extracted from the get_game_score function to answer questions around NBA game scores. Include the team and their opponent in your response.",
         tools,
-        //tool_choice: {type: "function", function: {name:"categorize"}},
+        tool_choice: {type: "function", function: {name:"categorize"}},
         verbose: true,
       },
       inputs: ["question.$1"],
