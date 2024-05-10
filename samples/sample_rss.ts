5b813b94041bda8201d2ba9bcc7e561c16cf80b9
snakajima <satoshi.nakajima@gmail.com>
2024-05-10 04:39:20 -0700

    more 0.3

--- samples/sample_rss.ts
+++ samples/sample_rss.ts
@@ -4,7 +4,7 @@ import { graphDataTestRunner } from "~/utils/runner";
 import { fetchAgent, propertyFilterAgent, groqAgent, stringTemplateAgent, copyAgent } from "@/experimental_agents";
 
 const graph_data = {
-  version: 0.2,
+  version: 0.3,
   nodes: {
     url: {
       value: "https://www.theverge.com/apple/rss/index.xml",
@@ -14,31 +14,30 @@ const graph_data = {
       params: {
         type: "xml",
       },
-      inputs: ["url"],
+      inputs: [":url"],
     },
     filter: {
       agent: "propertyFilterAgent",
       params: {
         include: ["title", "link", "content"],
       },
-      inputs: ["rssFeed.feed.entry"],
+      inputs: [":rssFeed.feed.entry"],
     },
     map: {
       agent: "mapAgent",
-      inputs: ["filter"],
+      inputs: [":filter"],
       isResult: true,
       params: {
         limit: 4, // to avoid rate limit
       },
       graph: {
-        version: 0.2,
         nodes: {
           template: {
             agent: "stringTemplateAgent",
             params: {
               template: "Title:${0}\n${1}",
             },
-            inputs: ["$0.title", "$0.content._"],
+            inputs: [":$0.title", ":$0.content._"],
           },
           query: {
             agent: "groqAgent",
@@ -46,12 +45,12 @@ const graph_data = {
               model: "Llama3-8b-8192", // "mixtral-8x7b-32768",
               query: "次のHTMLからテキストだけを抜き出し、省略せずに、全文を日本語に翻訳して。余計なことは言わずに、翻訳した文章だけ答えて。",
             },
-            inputs: ["template"],
+            inputs: [":template"],
           },
           extractor: {
             agent: "copyAgent",
             isResult: true,
-            inputs: ["query.choices.$0.message.content"],
+            inputs: [":query.choices.$0.message.content"],
           },
         },
       },
