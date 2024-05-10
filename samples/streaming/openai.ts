7587a8a3f2ecbd319d0d71c73271f20665d5c7b2
isamu <isamu@to-kyo.to>
2024-05-11 08:33:40 +0900

    add groq_stream_agent

--- samples/streaming/openai.ts
+++ samples/streaming/openai.ts
@@ -1,26 +1,8 @@
 import "dotenv/config";
 
 import { GraphAI } from "@/graphai";
-
-import { AgentFunctionContext } from "@/type";
 import { openAIAgent } from "@/experimental_agents/llm_agents/openai_agent";
-import { streamAgentFilterGenerator } from "@/experimental_agent_filters/stream";
-
-const streamData: Record<string, string> = {};
-
-const outSideFunciton = (context: AgentFunctionContext, data: string) => {
-  const nodeId = context.debugInfo.nodeId;
-  streamData[nodeId] = (streamData[nodeId] || "") + data;
-  console.log(streamData);
-};
-
-const agentFilters = [
-  {
-    name: "streamAgentFilter",
-    agent: streamAgentFilterGenerator<string>(outSideFunciton),
-  },
-];
-
+import { agentFilters } from "./streamAgentFilter";
 
 const graph_data = {
   version: 0.3,
--- src/experimental_agents/llm_agents/index.ts
+++ src/experimental_agents/llm_agents/index.ts
@@ -1,2 +1,3 @@
 export * from "./slashgpt_agent";
 export * from "./groq_agent";
+export * from "./groq_stream_agent";
