ee58bdf4da4e01db59974350b772b6b619ac4492
isamu <isamu@to-kyo.to>
2024-05-11 10:54:40 +0900

    format

--- samples/sample_runner.ts
+++ samples/sample_runner.ts
@@ -1,19 +1,38 @@
 import "dotenv/config";
 
+import { main as streaming_groq } from "./streaming/groq";
+import { main as streaming_openai } from "./streaming/openai";
+import { main as streaming_slashgpt } from "./streaming/slashgpt";
+import { main as streaming_openai_agent } from "./streaming/openai_agent";
+
 import { main as slashgpt } from "./llm/slashgpt";
+import { main as groq } from "./llm/groq";
 import { main as interaction_text } from "./interaction/text";
 import { main as interaction_select } from "./interaction/select";
 import { main as sample_co2 } from "./tools/sample_co2";
-import { main as sample_paper_ai } from "./sample_paper_ai";
-import { main as home } from "./home";
+import { main as tools_home } from "./tools/home";
+import { main as net_paper_ai } from "./net/paper_ai";
 
 const main = async () => {
+  // streaming
+  await streaming_groq();
+  await streaming_openai();
+  await streaming_slashgpt();
+  await streaming_openai_agent();
+
+  // llm
   await slashgpt();
+  await groq();
+  // interaction
   await interaction_text();
   await interaction_select();
+  // chat wikipedia
+
+  // tools
   await sample_co2();
-  await sample_paper_ai();
-  await home();
+  await tools_home();
+
+  await net_paper_ai();
 };
 
 main();
