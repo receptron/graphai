ca3fd161e9433b87303417c499b784a3a1fe55bc
snakajima <satoshi.nakajima@gmail.com>
2024-05-11 14:45:15 -0700

    compiles

--- tests/graphai/test_hooks.ts
+++ tests/graphai/test_hooks.ts
@@ -1,36 +1,45 @@
+import { GraphAI } from "@/graphai";
+import { defaultTestAgents } from "@/utils/test_agents";
 import { AgentFunction } from "@/graphai";
-import { graphDataTestRunner } from "~/utils/runner";
 import { sleep } from "@/utils/utils";
 
 import test from "node:test";
-import assert from "node:assert";
+// import assert from "node:assert";
 
 const graphdata_hook = {
   version: 0.3,
   nodes: {
     streamNode: {
       agent: "streamAgent",
+      params: {
+        stream: "~test_hook",
+      },
       isResult: true,
     },
   },
 };
 
 
-const streamAgent: AgentFunction<{ stream: (data:string)=> void }, string, any> = async ({
+const streamAgent: AgentFunction<{ stream: (data:Record<string, any>)=> void }, string, any> = async ({
   params,
 }) => {
   const message = "May the force be with you";
   if (params.stream) {
     message.split('').forEach(async (word: string) => {
       await sleep(10);
-      params.stream(word);    
+      params.stream({word});    
     });
   }
   return message;
 };
 
+const test_hook = (data: Record<string, any>) => {
+  console.log(data.word);
+}
+
 test("test dispatch", async () => {
-  const result = await graphDataTestRunner(__filename, graphdata_hook, {streamAgent}, () => {}, false);
+  const graph = new GraphAI(graphdata_hook, { ...defaultTestAgents, streamAgent }, { hooks: { test_hook }});
+  const result = await graph.run(false);  
   console.log(result);
   /*
   assert.deepStrictEqual(result, {
