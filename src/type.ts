83655a2db4cbddd5ca7a1405af819329f73a8895
isamu <isamu@to-kyo.to>
2024-05-12 18:17:29 +0900

    skip test

--- src/type.ts
+++ src/type.ts
@@ -109,6 +109,7 @@ export type AgentFunctionInfo = {
     params: DefaultParamsType;
     result: any;
   }[];
+  skipTest?: boolean;
   description: string;
   category: string[];
   author: string;
