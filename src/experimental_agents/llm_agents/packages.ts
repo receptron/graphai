70f69213d98b101c69c1b88b5bb42a26d720aa17
isamu <isamu@to-kyo.to>
2024-05-13 04:40:20 +0900

    fix typo

--- src/experimental_agents/llm_agents/packages.ts
+++ src/experimental_agents/llm_agents/packages.ts
@@ -1,5 +1,5 @@
 import groqAgent from "@/experimental_agents/llm_agents/groq_agent";
-import groqStreamAgent from "@experimental_agents/llm_agents/groq_stream_agent";
+import groqStreamAgent from "@/experimental_agents/llm_agents/groq_stream_agent";
 import slashGPTAgent from "@/experimental_agents/llm_agents/slashgpt_agent";
 import openAIAgent from "@/experimental_agents/llm_agents/openai_agent";
 
