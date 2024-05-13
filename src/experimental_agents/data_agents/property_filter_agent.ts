ceefe466be7a41ebf21209747baf9cbddf072d59
snakajima <satoshi.nakajima@gmail.com>
2024-05-13 05:25:55 -0700

    alter

--- src/experimental_agents/data_agents/property_filter_agent.ts
+++ src/experimental_agents/data_agents/property_filter_agent.ts
@@ -1,6 +1,6 @@
 import { AgentFunction } from "@/graphai";
 
-const applyFilter = (input: any, include: Array<string> | undefined, exclude: Array<string> | undefined) => {
+const applyFilter = (input: any, include: Array<string> | undefined, exclude: Array<string> | undefined, alter: Record<string, Record<string, string>> | undefined) => {
   const propIds = include ? include : Object.keys(input);
   const excludeSet = new Set(exclude ?? []);
   return propIds.reduce((tmp: Record<string, any>, propId) => {
@@ -11,13 +11,13 @@ const applyFilter = (input: any, include: Array<string> | undefined, exclude: Ar
   }, {});
 };
 
-export const propertyFilterAgent: AgentFunction<{ include?: Array<string>; exclude?: Array<string> }> = async ({ inputs, params }) => {
+export const propertyFilterAgent: AgentFunction<{ include?: Array<string>; exclude?: Array<string>; alter?: Record<string, Record<string, string>> }> = async ({ inputs, params }) => {
   const [input] = inputs;
-  const { include, exclude } = params;
+  const { include, exclude, alter } = params;
   if (Array.isArray(input)) {
-    return input.map((item) => applyFilter(item, include, exclude));
+    return input.map((item) => applyFilter(item, include, exclude, alter));
   }
-  return applyFilter(input, include, exclude);
+  return applyFilter(input, include, exclude, alter);
 };
 
 const inputs = [[
@@ -39,6 +39,14 @@ const propertyFilterAgentInfo = {
       inputs,
       params: { exclude: ["color", "model"] },
       result: [{ type: "EV", maker: "Tesla", range: 300 }, { type: "EV", maker: "Tesla", range: 400 }],
+    },
+    {
+      inputs,
+      params: { alter: { color: { red:"blue", blue:"red" } } },
+      result: [
+        { color: "red", model: "Model 3", type: "EV", maker: "Tesla", range: 300 },
+        { color: "blue", model: "Model Y", type: "EV", maker: "Tesla", range: 400 },
+      ],
     },
   ],
   description: "Filter properties based on property name either with 'include' or 'exclude'",
