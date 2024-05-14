6d859121a1dd38163b5d8ee602b3839e309101f3
snakajima <satoshi.nakajima@gmail.com>
2024-05-14 14:35:02 -0700

    messages

---
+++ samples/llm/gpt4o.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import { graphDataTestRunner } from "~/utils/runner";
+import { sleeperAgent, openAIAgent } from "@/experimental_agents";
+
+const query =
+  "I'd like to write a paper about data flow programming for AI application, which involves multiple asynchronous calls, some of operations are done on other machines (distributed computing). Please come up with the title and an abstract for this paper.";
+
+const graph_data = {
+  version: 0.3,
+  nodes: {
+    messages: {
+      value: [
+        {
+          role: "user",
+          content: query,
+        }
+      ],
+    },
+    query: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+      },
+      isResult: true,
+      inputs: [undefined, ":messages"]
+    },
+    answer: {
+      agent: "sleeperAgent",
+      inputs: [":query.choices.$0.message"],
+      isResult: true,
+    },
+  },
+};
+
+export const main = async () => {
+  const result = await graphDataTestRunner(__filename, graph_data, { openAIAgent, sleeperAgent });
+  console.log(result);
+};
+
+if (process.argv[1] === __filename) {
+  main();
+}
