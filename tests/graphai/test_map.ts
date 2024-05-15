54fc9c524c8cd5afb119ef1a1de7b1e4cacabd68
isamu <isamu@to-kyo.to>
2024-05-15 16:35:27 +0900

    test graph data to yaml

--- tests/graphai/test_map.ts
+++ tests/graphai/test_map.ts
@@ -1,41 +1,11 @@
-import { graphDataTestRunner } from "~/utils/runner";
+import { graphDataTestRunner, fileTestRunner } from "~/utils/runner";
 import { defaultTestAgents } from "@/utils/test_agents";
 
 import test from "node:test";
 import assert from "node:assert";
 
-const graphdata_push = {
-  version: 0.3,
-  nodes: {
-    source: {
-      value: { fruits: ["apple", "orange", "banana", "lemon", "melon", "pineapple", "tomato"] },
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: [":source.fruits"],
-      graph: {
-        version: 0.3,
-        nodes: {
-          node2: {
-            agent: "stringTemplateAgent",
-            params: {
-              template: "I love ${0}.",
-            },
-            inputs: [":$0"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "sleeperAgent",
-      inputs: [":nestedNode.node2"],
-    },
-  },
-};
-
 test("test map 1", async () => {
-  const result = await graphDataTestRunner("test_map", graphdata_push, defaultTestAgents);
+  const result = await fileTestRunner("/graphs/map/map1.yaml", defaultTestAgents);
   assert.deepStrictEqual(result.result, [
     "I love apple.",
     "I love orange.",
@@ -47,80 +17,14 @@ test("test map 1", async () => {
   ]);
 });
 
-const graphdata_map2 = {
-  version: 0.3,
-  nodes: {
-    source1: {
-      value: { fruit: "apple" },
-    },
-    source2: {
-      value: { fruit: "orange" },
-    },
-    source3: {
-      value: { fruit: "banana" },
-    },
-    source4: {
-      value: { fruit: "lemon" },
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: [":source1.fruit", ":source2.fruit", ":source3.fruit", ":source4.fruit"],
-      graph: {
-        version: 0.3,
-        nodes: {
-          node2: {
-            agent: "stringTemplateAgent",
-            params: {
-              template: "I love ${0}.",
-            },
-            inputs: [":$0"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "sleeperAgent",
-      inputs: [":nestedNode.node2"],
-    },
-  },
-};
-
 test("test map 2", async () => {
-  const result = await graphDataTestRunner("test_map2", graphdata_map2, defaultTestAgents);
+  const result = await fileTestRunner("/graphs/map/map2.yaml", defaultTestAgents);
   assert.deepStrictEqual(result.result, ["I love apple.", "I love orange.", "I love banana.", "I love lemon."]);
 });
 
 // nest graph and flat
-const graphdata_map3 = {
-  version: 0.3,
-  nodes: {
-    source1: {
-      value: ["hello", "hello2"],
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: [":source1"],
-      graph: {
-        version: 0.3,
-        nodes: {
-          node1: {
-            agent: "bypassAgent",
-            inputs: [":$0"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "bypassAgent",
-      inputs: [":nestedNode.node1"],
-    },
-  },
-};
-
 test("test map 3", async () => {
-  const result = await graphDataTestRunner("test_map3", graphdata_map3, defaultTestAgents);
+  const result = await fileTestRunner("/graphs/map/map3.yaml", defaultTestAgents);
   assert.deepStrictEqual(result.result, [[["hello"], ["hello2"]]]);
 });
 
