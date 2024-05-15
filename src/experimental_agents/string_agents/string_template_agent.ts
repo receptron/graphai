63d3bdfc488a2cb0cfa22234711ae7945e955b0c
snakajima <satoshi.nakajima@gmail.com>
2024-05-15 16:30:20 -0700

    array template support

--- src/experimental_agents/string_agents/string_template_agent.ts
+++ src/experimental_agents/string_agents/string_template_agent.ts
@@ -4,12 +4,17 @@ import { AgentFunction } from "@/index";
 //  tests/agents/test_string_agent.ts
 export const stringTemplateAgent: AgentFunction<
   {
-    template: string;
+    template: string | Array<string>;
   },
-  string,
+  string | Array<string>,
   string
 > = async ({ params, inputs }) => {
   const content = inputs.reduce((template, input, index) => {
+    if (Array.isArray(template)) {
+      return template.map((item) => {
+        return item.replace("${" + index + "}", input);
+      });
+    }
     return template.replace("${" + index + "}", input);
   }, params.template);
 
@@ -18,6 +23,7 @@ export const stringTemplateAgent: AgentFunction<
 
 const sampleInput = ["hello", "test"];
 const sampleParams = { template: "${0}: ${1}" };
+const sampleParamsArray = { template: ["${0}: ${1}"] };
 const sampleResult = "hello: test";
 
 // for test and document
@@ -30,6 +36,11 @@ const stringTemplateAgentInfo = {
       inputs: sampleInput,
       params: sampleParams,
       result: sampleResult,
+    },
+    {
+      inputs: sampleInput,
+      params: sampleParamsArray,
+      result: [sampleResult],
     },
   ],
   description: "Template agent",
