03791ad12359f8fbca1623cb681fe0ba6b955f7e
snakajima <satoshi.nakajima@gmail.com>
2024-05-15 19:02:24 -0700

    deep template

--- src/experimental_agents/string_agents/string_template_agent.ts
+++ src/experimental_agents/string_agents/string_template_agent.ts
@@ -1,36 +1,38 @@
 import { AgentFunction } from "@/index";
 
 const foo: any = (template: any, match: string, input: string) => {
+  console.log("---", template, match, input);
   if (typeof template === "string") {
     return template.replace(match, input);
   } else if (Array.isArray(template)) {
     return template.map((item: any) => foo(item, match, input));
   }
   return Object.keys(template).reduce((tmp:any, key:any) => {
-    tmp[key] = foo(template[key], match, input);
+    console.log("***", template, key);
+    const result = foo(template[key], match, input);
+    console.log("**-", result, tmp, key);
+    tmp[key] = result;
+    return tmp;
   }, {});
 }
 // see example
 //  tests/agents/test_string_agent.ts
 export const stringTemplateAgent: AgentFunction<
   {
-    template: string | Array<string>;
+    template: any;
   },
-  string | Array<string>,
+  any,
   string
 > = async ({ params, inputs }) => {
-  const content = inputs.reduce((template, input, index) => {
-    return foo(template, "${" + index + "}", input);
+  return inputs.reduce((template, input, index) => {
+    console.log("~~~", index, input);
+    const result = foo(template, "${" + index + "}", input);
+    console.log("~~+", result);
+    return result;
   }, params.template);
-
-  return content;
 };
 
 const sampleInput = ["hello", "test"];
-const sampleParams = { template: "${0}: ${1}" };
-const sampleParamsArray = { template: ["${0}: ${1}", "${1}: ${0}"] };
-const sampleResult = "hello: test";
-const sampleResultReversed = "test: hello";
 
 // for test and document
 const stringTemplateAgentInfo = {
@@ -40,13 +42,28 @@ const stringTemplateAgentInfo = {
   samples: [
     {
       inputs: sampleInput,
-      params: sampleParams,
-      result: sampleResult,
+      params: { template: "${0}: ${1}" },
+      result: "hello: test",
     },
     {
       inputs: sampleInput,
-      params: sampleParamsArray,
-      result: [sampleResult, sampleResultReversed],
+      params: { template: ["${0}: ${1}", "${1}: ${0}"] },
+      result: ["hello: test", "test: hello"],
+    },
+    {
+      inputs: sampleInput,
+      params: { template: { apple: "${0}", lemon: "${1}" } },
+      result: { apple: "hello", lemon: "test" },
+    },
+    {
+      inputs: sampleInput,
+      params: { template: [{ apple: "${0}", lemon: "${1}" }] },
+      result: [{ apple: "hello", lemon: "test" }],
+    },
+    {
+      inputs: sampleInput,
+      params: { template: { apple: "${0}", lemon: ["${1}"] } },
+      result: { apple: "hello", lemon: ["test"] },
     },
   ],
   description: "Template agent",
