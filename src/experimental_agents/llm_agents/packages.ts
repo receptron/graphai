4826caf48ec1fa0979401b917a186576e6c8f6bb
snakajima <satoshi.nakajima@gmail.com>
2024-05-16 09:58:16 -0700

    index and packages

--- src/experimental_agents/llm_agents/packages.ts
+++ src/experimental_agents/llm_agents/packages.ts
@@ -2,5 +2,6 @@ import groqAgent from "@/experimental_agents/llm_agents/groq_agent";
 import groqStreamAgent from "@/experimental_agents/llm_agents/groq_stream_agent";
 import slashGPTAgent from "@/experimental_agents/llm_agents/slashgpt_agent";
 import openAIAgent from "@/experimental_agents/llm_agents/openai_agent";
+import anthropicAgent from "@/experimental_agents/llm_agents/openai_agent";
 
-export { groqAgent, groqStreamAgent, slashGPTAgent, openAIAgent };
+export { groqAgent, groqStreamAgent, slashGPTAgent, openAIAgent, anthropicAgent };
