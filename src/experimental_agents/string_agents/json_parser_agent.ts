b09600e7b3b25622f4cbb6e996dc36289762fd5c
snakajima <satoshi.nakajima@gmail.com>
2024-05-17 05:41:44 -0700

    jsonParserAgent

---
+++ src/experimental_agents/string_agents/json_parser_agent.ts
@@ -0,0 +1,40 @@
+import { AgentFunction } from "@/index";
+
+export const jsonParserAgent: AgentFunction<
+  {
+    stringify: boolean
+  },
+  any,
+  any
+> = async ({ params, inputs }) => {
+  if (params.stringify) {
+    return JSON.stringify(inputs[0], null, 2);
+  }  
+  return JSON.parse(inputs[0]);
+};
+
+const sample_object = { apple:"red", lemon: "yellow" };
+// for test and document
+const jsonParserAgentInfo = {
+  name: "jsonParserAgent",
+  agent: jsonParserAgent,
+  mock: jsonParserAgent,
+  samples: [
+    {
+      inputs: [sample_object],
+      params: { stringify: true },
+      result: JSON.stringify(sample_object, null, 2),
+    },
+    {
+      inputs: [JSON.stringify(sample_object, null, 2)],
+      params: {},
+      result: sample_object,
+    },
+  ],
+  description: "Template agent",
+  category: ["string"],
+  author: "Satoshi Nakajima",
+  repository: "https://github.com/receptron/graphai",
+  license: "MIT",
+};
+export default jsonParserAgentInfo;
