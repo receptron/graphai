3cfece76b12d14e82d8a2a928db0becbe35ecfc3
snakajima <satoshi.nakajima@gmail.com>
2024-05-17 11:05:12 -0700

    worker thread

--- src/experimental_agents/graph_agents/worker_agent.ts
+++ src/experimental_agents/graph_agents/worker_agent.ts
@@ -1,6 +1,24 @@
 import { GraphAI, AgentFunction } from "@/index";
 import { assert } from "@/utils/utils";
 import { getNestedGraphData } from "./nested_agent";
+import { Worker } from "worker_threads";
+
+const workerFunction = () => {
+  //we perform every operation we want in this function right here
+  self.onmessage = (event: MessageEvent) => {
+    console.log(event.data);
+    postMessage('Message has been gotten!');
+  };
+};
+const workerCode = workerFunction.toString();
+const index = workerCode.indexOf('{');
+const codeBlock = workerCode.slice(index);
+let blob = new Blob([codeBlock], { type: 'application/javascript' });
+let worker_script = URL.createObjectURL(blob);
+console.log(worker_script);
+
+//assert(!!match, "foo");
+//console.log(match[1]);
 
 export const workerAgent: AgentFunction<
   {
@@ -8,6 +26,10 @@ export const workerAgent: AgentFunction<
   any,
   any
 > = async ({ inputs, agents, log, graphData }) => {
+  const worker = new Worker("./worker.js");
+  console.log(worker);
+  
+
   return { message: "Hello World" };
 };
 
