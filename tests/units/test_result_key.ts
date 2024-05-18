faa7438cbd811ced1f42443911f337d27b0bedf4
isamu <isamu@to-kyo.to>
2024-05-18 11:26:19 +0900

    cleaup

--- tests/units/test_result_key.ts
+++ tests/units/test_result_key.ts
@@ -1,43 +1,24 @@
 import test from "node:test";
 import assert from "node:assert";
 
-const objectJoin = (innerData: any) => {
-  const ret: any = [];
-  Object.keys(innerData).forEach((key: any) => {
+const objectToKeyArray = (innerData: any) => {
+  const ret: string[][] = [];
+  Object.keys(innerData).forEach((key: string) => {
     if (Object.keys(innerData[key]).length === 0) {
-      ret.push(key)
+      ret.push([key])
     } else {
-      const hoge = objectJoin(innerData[key]) 
-      if (Array.isArray(hoge)) {
-        hoge.forEach((tmp: any) => {
-          if (Array.isArray(tmp)) {
-            const arr = [key, ...tmp]
-            ret.push(arr)
-          } else {
-            const arr = [key, tmp]
-            ret.push(arr)
-          }
-        });
-      } else {
-        const arr = [key, ...hoge]
-        ret.push(arr)
-      }
+      objectToKeyArray(innerData[key]).forEach((tmp: string[]) => {
+        ret.push([key, ...tmp])
+      });
     }
   });
   return ret;
 };
 
-
 const debugResultKey = (agentId: string, result: any) => {
-  const inner = debugResultKeyInner(result);
-  const res = {};
-  const hoge = objectJoin({[agentId]: inner});
-  return (hoge.map((a: string[]) => {
-    if (Array.isArray(a)) {
-      return a.join(".")
-    }
-    return a;
-  })).map((a: string) => ":" + a);
+  return objectToKeyArray({[agentId]: debugResultKeyInner(result)}).map((objectKeys: string[]) => {
+    return ":" + objectKeys.join(".")
+  })
 };
 
 const debugResultKeyInner = (result: any) => {
@@ -45,12 +26,12 @@ const debugResultKeyInner = (result: any) => {
     return {};
   }
   if (Array.isArray(result)) {
-    return Array.from(result.keys()).reduce((tmp: any, index: any) => {
-      tmp["$" + index] = debugResultKeyInner(result[index]);
+    return Array.from(result.keys()).reduce((tmp: Record<string, any>, index: number) => {
+      tmp["$" + String(index)] = debugResultKeyInner(result[index]);
       return tmp;
     }, {});
   }
-  return Object.keys(result).reduce((tmp: any, key: any) => {
+  return Object.keys(result).reduce((tmp: Record<string, any>, key: string) => {
     tmp[key] = debugResultKeyInner(result[key]);
     return tmp;
   }, {});
@@ -74,16 +55,7 @@ test("test object", async () => {
   const agentId = "agentABC";
   const result = { data: [1, 2], data2: {hoge: "aa", foo: {bar: {boo: "bb"}}} };
   const res = debugResultKey(agentId, result);
-  // console.log(res)
   assert.deepStrictEqual(res, [":agentABC.data.$0", ":agentABC.data.$1", ":agentABC.data2.hoge", ":agentABC.data2.foo.bar.boo"]);
 });
 
 
-// { data: { '$0': {}, '$1': {} }, data2: { hoge: {}, foo: {} } }
-[
-  "data.$0",
-  "data.$1",
-  "data2.hoge",
-  "data2.foo",
-
-  ]
