10eac4db81bdb68a04bb18b204a6da28bbd43ba3
snakajima <satoshi.nakajima@gmail.com>
2024-05-20 04:01:46 -0700

    reception

---
+++ samples/interaction/reception.ts
@@ -0,0 +1,141 @@
+import "dotenv/config";
+import { graphDataTestRunner } from "~/utils/runner";
+import { openAIAgent, shiftAgent, nestedAgent, textInputAgent, propertyFilterAgent, stringTemplateAgent } from "@/experimental_agents";
+
+const tools = [{
+  type: "function",
+  function: {
+    "name": "report",
+    "description": "Report the information acquired from the user",
+    "parameters": {
+      "type": "object",
+      "properties": {
+        "name": {
+          "type": "string",
+          "description": "the name of the patient (first and last)"
+        },
+        "sex": {
+          "type": "string",
+          "description": "Gender of the patient.",
+          "enum": ["male", "female"]
+        },
+        "dob": {
+          "type": "string",
+          "description": "Patient's date of birth."
+        }
+      },
+      "required": ["name", "sex", "dob"]
+    }
+  }
+}];
+
+const graph_data = {
+  version: 0.3,
+  loop: {
+    while: ":continue",
+  },
+  nodes: {
+    // Holds a boolean value, which specifies if we need to contine or not.
+    continue: {
+      value: true,
+      update: ":checkInput.continue",
+    },
+    messages: {
+      // Holds the conversation, the array of messages.
+      value: [{ role: "system", content: "You  are responsible in retrieving following information from the user.\n" +
+      "name: both first and last name\n" +
+      "dob: date of birth. It MUST include the year\n" +
+      "sex: gender (NEVER guess from the name)\n" +
+      "When you get all the information from the user, call the function 'report'.\n" }],
+      update: ":reducer",
+      isResult: true,
+    },
+    userInput: {
+      // Receives an input from the user.
+      agent: "textInputAgent",
+      params: {
+        message: "You:",
+      },
+    },
+    checkInput: {
+      // Checks if the user is willing to terminate the chat or not.
+      agent: "propertyFilterAgent",
+      params: {
+        inspect: [
+          {
+            propId: "continue",
+            notEqual: "/bye",
+          },
+        ],
+      },
+      inputs: [{}, ":userInput"],
+    },
+    userMessage: {
+      // Generates an message object with the user input.
+      agent: "propertyFilterAgent",
+      params: {
+        inject: [{
+          propId: "content",
+          from: 1
+        }]        
+      },
+      inputs: [{ role: "user" }, ":userInput"],
+    },
+    appendedMessages: {
+      // Appends it to the conversation
+      agent: "pushAgent",
+      inputs: [":messages", ":userMessage"],
+      if: ":checkInput.continue",
+    },
+    llm: {
+      // Sends those messages to LLM to get a response.
+      console: {
+        after: true,
+      },
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        tools
+      },
+      inputs: [undefined, ":appendedMessages"],
+    },
+    output: {
+      // Displays the response to the user.
+      agent: "stringTemplateAgent",
+      params: {
+        template: "\x1b[32mLlama3\x1b[0m: ${0}"
+      },
+      console: {
+        after: true,
+      },
+      inputs: [":llm.choices.$0.message.content"],
+    },
+    reducer: {
+      // Appends the responce to the messages.
+      agent: "pushAgent",
+      inputs: [":appendedMessages", ":llm.choices.$0.message"],
+    },
+  },
+};
+
+export const main = async () => {
+  const result = await graphDataTestRunner(
+    __filename,
+    graph_data,
+    {
+      openAIAgent,
+      shiftAgent,
+      nestedAgent,
+      textInputAgent,
+      propertyFilterAgent,
+      stringTemplateAgent,
+    },
+    () => {},
+    false,
+  );
+  console.log("Complete", result);
+};
+
+if (process.argv[1] === __filename) {
+  main();
+}
