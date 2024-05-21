5a3dc98a9f7539158bd846fd1d4b2970c9799227
snakajima <satoshi.nakajima@gmail.com>
2024-05-21 04:45:37 -1000

    graph_tool

--- samples/net/weather.ts
+++ samples/net/weather.ts
@@ -27,6 +27,111 @@ const tools = [
   },
 ];
 
+const graph_tool = {
+  nodes: {
+    outputFetching: {
+      // Displays the fetching message.
+      agent: "stringTemplateAgent",
+      params: {
+        template: "... fetching weather info: ${0}",
+      },
+      console: {
+        after: true,
+      },
+      inputs: [":$0.$0.function.arguments"],
+    },
+    parser: {
+      // Parse the arguments to the function
+      agent: "jsonParserAgent",
+      inputs: [":$0.$0.function.arguments"]
+    },
+    urlPoints: {
+      // Builds a URL to fetch the "grid location" from the spcified latitude and longitude
+      agent: "stringTemplateAgent",
+      params: {
+        template: "https://api.weather.gov/points/${0},${1}",
+      },
+      inputs: [":parser.latitude", ":parser.longitude"],
+    },
+    fetchPoints: {
+      // Fetches the "grid location" from the URL.
+      agent: "fetchAgent",
+      params: {
+        returnErrorResult: true, // returns {status error} in case of error
+      },
+      inputs: [":urlPoints", undefined, { "User-Agent": "(receptron.org)" }],
+    },
+    fetchForecast: {
+      // Fetches the weather forecast for that location.
+      agent: "fetchAgent",
+      params: {
+        type: "text",
+      },
+      inputs: [":fetchPoints.properties.forecast", undefined, { "User-Agent": "(receptron.org)" }],
+      if: ":fetchPoints.properties.forecast",
+    },
+    extractError: {
+      // Extract error title and detail
+      agent: "stringTemplateAgent",
+      params: {
+        template: "${0}: ${1}",
+      },
+      inputs: [":fetchPoints.error.title", ":fetchPoints.error.detail"],
+      if: ":fetchPoints.error",
+    },
+    responseText: {
+      // Extract the forecast and error
+      agent: "copyAgent",
+      anyInput: true,
+      inputs: [":fetchForecast", ":extractError"],
+    },
+    toolMessage: {
+      // Creates a tool message as the return value of the tool call.
+      agent: "propertyFilterAgent",
+      params: {
+        inject: [{
+          propId: "tool_call_id",
+          from: 1,
+        }, {
+          propId: "name",
+          from: 2,
+        }, {
+          propId: "content",
+          from: 3,
+        }]
+      },
+      inputs: [{ role: "tool" }, ":$0.$0.id", ":$0.$0.function.name", ":responseText"],
+    },
+    messagesWithToolRes: {
+      // Appends that message to the messages.
+      agent: "pushAgent",
+      inputs: [":$1", ":toolMessage"],
+    },
+    llmCall: {
+      // Sends those messages to LLM to get the answer.
+      agent: "openAIAgent",
+      inputs: [undefined, ":messagesWithToolRes"],
+    },
+    output: {
+      // Displays the response to the user.
+      agent: "stringTemplateAgent",
+      params: {
+        template: "Weather: ${0}",
+      },
+      console: {
+        after: true,
+      },
+      inputs: [":llmCall.choices.$0.message.content"],
+    },
+    messagesWithSecondRes: {
+      // Appends the response to the messages.
+      agent: "pushAgent",
+      inputs: [":messagesWithToolRes", ":llmCall.choices.$0.message"],
+      isResult: true,
+    },
+  },
+};
+
 const graph_data = {
   version: 0.3,
   loop: {
@@ -108,117 +213,12 @@ const graph_data = {
       agent: "pushAgent",
       inputs: [":messagesWithUserInput", ":llmCall.choices.$0.message"],
     },
-
     tool_calls: {
       // This node is activated if the LLM requests a tool call.
       agent: "nestedAgent",
       inputs: [":llmCall.choices.$0.message.tool_calls", ":messagesWithFirstRes"],
       if: ":llmCall.choices.$0.message.tool_calls",
-      graph: {
-        // This graph is nested only for the readability.
-        nodes: {
-          outputFetching: {
-            // Displays the fetching message.
-            agent: "stringTemplateAgent",
-            params: {
-              template: "... fetching weather info: ${0}",
-            },
-            console: {
-              after: true,
-            },
-            inputs: [":$0.$0.function.arguments"],
-          },
-          parser: {
-            // Parse the arguments to the function
-            agent: "jsonParserAgent",
-            inputs: [":$0.$0.function.arguments"]
-          },
-          urlPoints: {
-            // Builds a URL to fetch the "grid location" from the spcified latitude and longitude
-            agent: "stringTemplateAgent",
-            params: {
-              template: "https://api.weather.gov/points/${0},${1}",
-            },
-            inputs: [":parser.latitude", ":parser.longitude"],
-          },
-          fetchPoints: {
-            // Fetches the "grid location" from the URL.
-            agent: "fetchAgent",
-            params: {
-              returnErrorResult: true, // returns {status error} in case of error
-            },
-            inputs: [":urlPoints", undefined, { "User-Agent": "(receptron.org)" }],
-          },
-          fetchForecast: {
-            // Fetches the weather forecast for that location.
-            agent: "fetchAgent",
-            params: {
-              type: "text",
-            },
-            inputs: [":fetchPoints.properties.forecast", undefined, { "User-Agent": "(receptron.org)" }],
-            if: ":fetchPoints.properties.forecast",
-          },
-          extractError: {
-            // Extract error title and detail
-            agent: "stringTemplateAgent",
-            params: {
-              template: "${0}: ${1}",
-            },
-            inputs: [":fetchPoints.error.title", ":fetchPoints.error.detail"],
-            if: ":fetchPoints.error",
-          },
-          responseText: {
-            // Extract the forecast and error
-            agent: "copyAgent",
-            anyInput: true,
-            inputs: [":fetchForecast", ":extractError"],
-          },
-          toolMessage: {
-            // Creates a tool message as the return value of the tool call.
-            agent: "propertyFilterAgent",
-            params: {
-              inject: [{
-                propId: "tool_call_id",
-                from: 1,
-              }, {
-                propId: "name",
-                from: 2,
-              }, {
-                propId: "content",
-                from: 3,
-              }]
-            },
-            inputs: [{ role: "tool" }, ":$0.$0.id", ":$0.$0.function.name", ":responseText"],
-          },
-          messagesWithToolRes: {
-            // Appends that message to the messages.
-            agent: "pushAgent",
-            inputs: [":$1", ":toolMessage"],
-          },
-          llmCall: {
-            // Sends those messages to LLM to get the answer.
-            agent: "openAIAgent",
-            inputs: [undefined, ":messagesWithToolRes"],
-          },
-          output: {
-            // Displays the response to the user.
-            agent: "stringTemplateAgent",
-            params: {
-              template: "Weather: ${0}",
-            },
-            console: {
-              after: true,
-            },
-            inputs: [":llmCall.choices.$0.message.content"],
-          },
-          messagesWithSecondRes: {
-            // Appends the response to the messages.
-            agent: "pushAgent",
-            inputs: [":messagesWithToolRes", ":llmCall.choices.$0.message"],
-            isResult: true,
-          },
-        },
-      },
+      graph: graph_tool,
     },
     no_tool_calls: {
       // This node is activated only if this is a normal response (not a tool call).
@@ -226,7 +226,6 @@ const graph_data = {
       unless: ":llmCall.choices.$0.message.tool_calls",
       inputs: [":messagesWithFirstRes"],
     },
-
     reducer: {
       // Receives messages from either case.
       agent: "copyAgent",
