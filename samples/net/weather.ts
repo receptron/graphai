92d487982b304023ff0f8a7473d9a0817f86bfda
snakajima <satoshi.nakajima@gmail.com>
2024-05-21 04:46:03 -1000

    eslint

--- samples/net/weather.ts
+++ samples/net/weather.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import { graphDataTestRunner } from "~/utils/runner";
 import { groqAgent, openAIAgent, nestedAgent, copyAgent, fetchAgent, textInputAgent, jsonParserAgent, propertyFilterAgent } from "@/experimental_agents";
-import input from "@inquirer/input";
 
 const tools = [
   {
