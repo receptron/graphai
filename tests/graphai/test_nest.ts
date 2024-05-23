d351a1d3b68a254c40c981085ccf2db196e8f718
snakajima <satoshi.nakajima@gmail.com>
2024-05-22 15:48:21 -1000

    test mapAgent

--- tests/graphai/test_nest.ts
+++ tests/graphai/test_nest.ts
@@ -1,6 +1,6 @@
 import { GraphAI } from "@/index";
 import { graphDataTestRunner } from "~/utils/runner";
-import { nestedAgent, copyAgent, propertyFilterAgent } from "@/experimental_agents";
+import { nestedAgent, copyAgent, propertyFilterAgent, mapAgent } from "@/experimental_agents";
 
 import test from "node:test";
 import assert from "node:assert";
@@ -95,3 +95,42 @@ test("test nest invalid 2", async () => {
     message: "\x1B[41mInvalid Agent : invalidAgent is not in AgentFunctionInfoDictionary.\x1B[0m"
   });
 });
+
+const graphdata_mapped = {
+  version: 0.3,
+  nodes: {
+    source: {
+      value: valid_graph,
+    },
+    array: {
+      value: [1, 2, 3]
+    },
+    nested: {
+      agent: "mapAgent",
+      graph: ":source",
+      isResult: true,
+      inputs: [":array"],
+    },
+    catch: {
+      agent: "propertyFilterAgent",
+      params: {
+        include: ["message"]
+      },
+      if: ":nested.onError",
+      inputs: [":nested.onError"],
+      isResult: true,
+    }
+  },
+};
+
+test("test map invalid", async () => {
+  const result = await graphDataTestRunner("test_map_invalid", graphdata_mapped, { mapAgent, copyAgent, propertyFilterAgent },
+    (graph: GraphAI) => {
+      graph.injectValue("source", invalid_graph);
+    }, false);
+  assert.deepStrictEqual(result.catch, {
+    message: "\x1B[41mInputs not match: NodeId result, Inputs: badsource\x1B[0m"
+  });
+});
+
+
