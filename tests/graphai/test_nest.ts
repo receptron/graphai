b991f4b0dd16ceef5c8d32eade7d3aa6e3c00776
snakajima <satoshi.nakajima@gmail.com>
2024-05-22 15:15:58 -1000

    valid graph

---
+++ tests/graphai/test_nest.ts
@@ -0,0 +1,54 @@
+import { graphDataTestRunner } from "~/utils/runner";
+import { nestedAgent, copyAgent } from "@/experimental_agents";
+
+import test from "node:test";
+import assert from "node:assert";
+
+const valid_graph = {
+  nodes: {
+    source: {
+      value: 1
+    },
+    result: {
+      agent: "copyAgent",
+      inputs: [":source"],
+      isResult: true,
+    }
+  }
+};
+
+const invalid_graph = {
+  nodes: {
+    source: {
+      value: 1
+    },
+    result: {
+      agent: "copyAgent",
+      inputs: [":badsource"],
+      isResult: true,
+    }
+  }
+};
+
+const graphdata_nest_valid = {
+  version: 0.3,
+  nodes: {
+    source: {
+      value: valid_graph,
+    },
+    nested: {
+      agent: "nestedAgent",
+      graph: ":source",
+      isResult: true,
+    },
+  },
+};
+
+test("test nest valid", async () => {
+  const result = await graphDataTestRunner("test_nest_valid", graphdata_nest_valid, { nestedAgent, copyAgent }, ()=>{}, false);
+  assert.deepStrictEqual(result, {
+    nested: {
+      result: 1
+    }
+  });
+});
