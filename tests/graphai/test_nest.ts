d361ef1d7303f7b90a211d24febbb7344007f901
snakajima <satoshi.nakajima@gmail.com>
2024-05-22 15:39:44 -1000

    inject

--- tests/graphai/test_nest.ts
+++ tests/graphai/test_nest.ts
@@ -1,5 +1,6 @@
+import { GraphAI } from "@/index";
 import { graphDataTestRunner } from "~/utils/runner";
-import { nestedAgent, copyAgent } from "@/experimental_agents";
+import { nestedAgent, copyAgent, propertyFilterAgent } from "@/experimental_agents";
 
 import test from "node:test";
 import assert from "node:assert";
@@ -17,6 +18,38 @@ const valid_graph = {
   }
 };
 
+const graphdata_nested = {
+  version: 0.3,
+  nodes: {
+    source: {
+      value: valid_graph,
+    },
+    nested: {
+      agent: "nestedAgent",
+      graph: ":source",
+      isResult: true,
+    },
+    catch: {
+      agent: "propertyFilterAgent",
+      params: {
+        include: ["message"]
+      },
+      if: ":nested.onError",
+      inputs: [":nested.onError"],
+      isResult: true,
+    }
+  },
+};
+
+test("test nest valid", async () => {
+  const result = await graphDataTestRunner("test_nest_valid", graphdata_nested, { nestedAgent, copyAgent, propertyFilterAgent }, ()=>{}, false);
+  assert.deepStrictEqual(result, {
+    nested: {
+      result: 1
+    }
+  });
+});
+
 const invalid_graph = {
   nodes: {
     source: {
@@ -30,48 +63,25 @@ const invalid_graph = {
   }
 };
 
-const graphdata_nest_valid = {
-  version: 0.3,
+const invalid_graph2 = {
   nodes: {
     source: {
-      value: valid_graph,
+      agent: "invalidAgent"
     },
-    nested: {
-      agent: "nestedAgent",
-      graph: ":source",
+    result: {
+      agent: "copyAgent",
+      inputs: [":badsource"],
       isResult: true,
-    },
-  },
+    }
+  }
 };
 
-test("test nest valid", async () => {
-  const result = await graphDataTestRunner("test_nest_valid", graphdata_nest_valid, { nestedAgent, copyAgent }, ()=>{}, false);
-  assert.deepStrictEqual(result, {
-    nested: {
-      result: 1
-    }
-  });
-});
-
-const graphdata_nest_invalid = {
-  version: 0.3,
-  nodes: {
-    source: {
-      value: invalid_graph,
-    },
-    nested: {
-      agent: "nestedAgent",
-      graph: ":source",
-      isResult: true,
-    },
-  },
-};
-
-test("test nest valid", async () => {
-  const result = await graphDataTestRunner("test_nest_valid", graphdata_nest_invalid, { nestedAgent, copyAgent }, ()=>{}, false);
-  assert.deepStrictEqual(result, {
-    nested: {
-      result: 1
-    }
+test("test nest invalid", async () => {
+  const result = await graphDataTestRunner("test_nest_valid", graphdata_nested, { nestedAgent, copyAgent, propertyFilterAgent },
+    (graph: GraphAI) => {
+      graph.injectValue("source", invalid_graph);
+    }, false);
+  assert.deepStrictEqual(result.catch, {
+    message: "\x1B[41mInputs not match: NodeId result, Inputs: badsource\x1B[0m"
   });
 });
