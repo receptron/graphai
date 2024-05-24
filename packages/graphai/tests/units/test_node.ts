d347a6a4a7769fc7e81cefec0ee55d4650690c48
isamu <isamu@to-kyo.to>
2024-05-24 20:57:47 +0900

    update test

--- packages/graphai/tests/units/test_node.ts
+++ packages/graphai/tests/units/test_node.ts
@@ -2,14 +2,13 @@ import { GraphAI } from "@/index";
 import { StaticNode } from "@/node";
 import { NodeState } from "@/type";
 
-import { defaultTestAgents } from "@/utils/test_agents";
 import { graph_data } from "~/units/graph_data";
 
 import test from "node:test";
 import assert from "node:assert";
 
 test("test static node injection", async () => {
-  const graph = new GraphAI(graph_data, defaultTestAgents);
+  const graph = new GraphAI(graph_data, {}, {bypassAgentIds: ["echoAgent", "bypassAgent"]});
   const nodeId = "nodeId";
   const node = new StaticNode(nodeId, { value: { data: "123" } }, graph);
   assert.deepStrictEqual(node.value, { data: "123" });
