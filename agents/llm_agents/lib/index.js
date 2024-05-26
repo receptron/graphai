f27be6014553214a6c29d006d1336a32a948f2ad
isamu <isamu@to-kyo.to>
2024-05-26 15:01:20 +0900

    agent

---
+++ agents/llm_agents/lib/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.geminiAgent = exports.anthropicAgent = exports.openAIAgent = exports.slashGPTAgent = exports.groqAgent = void 0;
+const groq_agent_1 = __importDefault(require("./groq_agent"));
+exports.groqAgent = groq_agent_1.default;
+const slashgpt_agent_1 = __importDefault(require("./slashgpt_agent"));
+exports.slashGPTAgent = slashgpt_agent_1.default;
+const openai_agent_1 = __importDefault(require("./openai_agent"));
+exports.openAIAgent = openai_agent_1.default;
+const anthropic_agent_1 = __importDefault(require("./anthropic_agent"));
+exports.anthropicAgent = anthropic_agent_1.default;
+const gemini_agent_1 = __importDefault(require("./gemini_agent"));
+exports.geminiAgent = gemini_agent_1.default;
