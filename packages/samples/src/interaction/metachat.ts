0c4d247c8c186e45b693f51d46e163e841c299de
snakajima <satoshi.nakajima@gmail.com>
2024-05-25 20:14:21 -1000

    messages as node

--- packages/samples/src/interaction/metachat.ts
+++ packages/samples/src/interaction/metachat.ts
@@ -8,37 +8,41 @@ import * as sample from "./reception";
 const filePath = path.join(__dirname, "../../../../README.md");
 const document = fs.readFileSync(filePath, "utf8");
 
-const messages = [
-  {
-    // System message, which gives the specification of GraphAI, and instruct to generate a graphAI graph dynamically.
-    role: "system",
-    content:
-      "You an expert in GraphAI programming. You are responsible in generating a graphAI graph to get required information from the user.\n" +
-      "[documation of GraphAI]\n" +
-      document,
-  },
-  {
-    // Sample question, which specifies which information we need to get from the user.
-    role: "user",
-    content: "Name, Date of Birth and Gendar",
-  },
-  {
-    // Sample AI agent graph, which acquires those information from the user.
-    role: "assistant",
-    content: "```json\n" + JSON.stringify(sample.graph_data) + "```\n",
-  },
-];
-
 export const graph_data = {
   version: 0.3,
   nodes: {
+    messages: {
+      agent: "stringTemplateAgent",
+      params: {
+          template: [
+            {
+              // System message, which gives the specification of GraphAI, and instruct to generate a graphAI graph dynamically.
+              role: "system",
+              content:
+                "You an expert in GraphAI programming. You are responsible in generating a graphAI graph to get required information from the user.\n" +
+                "[documation of GraphAI]\n${0}",
+            },
+            {
+              // Sample question, which specifies which information we need to get from the user.
+              role: "user",
+              content: "Name, Date of Birth and Gendar",
+            },
+            {
+              // Sample AI agent graph, which acquires those information from the user.
+              role: "assistant",
+              content: "```json\n${1}```\n",
+            },
+          ]
+      },
+      inputs: [document, JSON.stringify(sample.graph_data)]
+    },
     graphGenerator: {
       // Generates a graph for an AI agent to acquire specified information from the user.
       agent: "openAIAgent",
       params: {
         model: "gpt-4o",
       },
-      inputs: ["Name, Address and Phone Number", messages],
+      inputs: ["Name, Address and Phone Number", ":messages"],
     },
     parser: {
       // Parses the JSON data in the returned message
