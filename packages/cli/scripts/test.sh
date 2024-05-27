628bcd409f34541af914463ebf44f76413fc5488
isamu <isamu@to-kyo.to>
2024-05-27 09:39:56 +0900

    update version

--- packages/cli/scripts/test.sh
+++ packages/cli/scripts/test.sh
@@ -6,6 +6,6 @@ node lib/graphai_cli.js test_yaml/map1.yaml
 node lib/graphai_cli.js test_yaml/test_base.yml
 node lib/graphai_cli.js test_yaml/test_base.yml -v
 yarn run cli -l
-yarn run cli -d slashGPTAgentInfo
-yarn run cli -s stringEmbeddingsAgentInfo
+yarn run cli -d slashGPTAgent
+yarn run cli -s stringEmbeddingsAgent
 LC_ALL=en.us yarn run cli --help
