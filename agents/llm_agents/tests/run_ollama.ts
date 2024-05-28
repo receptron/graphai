067b164a0546028baa763d3e77f16cc41ac118f1
isamu <isamu@to-kyo.to>
2024-05-29 08:53:26 +0900

    format

--- agents/llm_agents/tests/run_ollama.ts
+++ agents/llm_agents/tests/run_ollama.ts
@@ -13,11 +13,10 @@ test("test agent filter", async () => {
     baseURL: "http://127.0.0.1:11434/v1",
     model,
     apiKey: model,
-  }
-  const res = await openAIAgent({ inputs, params, filterParams: {}, debugInfo: {verbose: false, nodeId: "test", retry: 5}}) as any;
+  };
+  const res = (await openAIAgent({ inputs, params, filterParams: {}, debugInfo: { verbose: false, nodeId: "test", retry: 5 } })) as any;
   if (res) {
     console.log(res.choices[0]);
   }
   assert.deepStrictEqual(true, true);
-  
 });
