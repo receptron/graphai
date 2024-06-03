364ead47b022de97a334f989394e380533cb1ec2
snakajima <satoshi.nakajima@gmail.com>
2024-06-03 15:02:03 -0700

    Answer3

--- docs/async/sample1.ts
+++ docs/async/sample1.ts
@@ -9,6 +9,19 @@ const Answer1 = async () => {
   return await FuncF(d, e);
 }
 
+const Answer2 = async () => {
+  const [a, b, c] = await Promise.all([FuncA(), FuncB(), FuncC()]); 
+  const d = await FuncD(a, b);
+  const e = await FuncE(b, c);
+  return await FuncF(d, e);
+}
+
+const Answer3 = async () => {
+  const [a, b, c] = await Promise.all([FuncA(), FuncB(), FuncC()]); 
+  const [d, e] = await Promise.all([FuncD(a,b), FuncE(b,c)]);
+  return await FuncF(d, e);
+}
+
 const timer = async (p: Promise<any>) => {
   const now = Date.now();
   const result = await p;
@@ -17,6 +30,8 @@ const timer = async (p: Promise<any>) => {
 
 const main = async () => {
   console.log(await timer(Answer1()));
+  console.log(await timer(Answer2()));
+  console.log(await timer(Answer3()));
 };
 
 if (process.argv[1] === __filename) {
