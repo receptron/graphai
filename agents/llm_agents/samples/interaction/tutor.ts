a48d7f41465f514925de3ea45fe8c01c79fd39fe
snakajima <satoshi.nakajima@gmail.com>
2024-06-04 14:25:05 -0700

    jsonParser

--- agents/llm_agents/samples/interaction/tutor.ts
+++ agents/llm_agents/samples/interaction/tutor.ts
@@ -18,6 +18,7 @@ export const graph_data = {
     meaning_llm: {
       agent: "openAIAgent",
       params: {
+        model: "gpt-4o",
         system: "You are a dictionary writer. Write the meaning of the given word."
       },
       inputs: [":word"]
@@ -30,6 +31,7 @@ export const graph_data = {
     meaning_jp_llm: {
       agent: "openAIAgent",
       params: {
+        model: "gpt-4o",
         system: "あなたは英語の教師です。与えられた単語の意味を日本語で説明してください。"
       },
       inputs: [":word"]
@@ -49,9 +51,36 @@ export const graph_data = {
       inputs: [":word"]
     },
     samples: {
-      agent: "copyAgent",
+      agent: "jsonParserAgent",
       isResult: true,
       inputs: [":samples_llm.choices.$0.message.content"]
+    },
+    similar_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        system: "与えられた単語をと類似するいくつか単語を並べて、日本語で違いを説明して。フォーマットはJSONで、以下のフォーマットで。\n"
+          + "```json\n[{word:'Awesome.', jp:'本当に素晴らしいことを強調したい時に使います。']\n```"
+      },
+      inputs: [":word"]
+    },
+    similar: {
+      agent: "jsonParserAgent",
+      isResult: true,
+      inputs: [":similar_llm.choices.$0.message.content"]
+    },
+    root_llm: {
+      agent: "openAIAgent",
+      params: {
+        model: "gpt-4o",
+        system: "あなたは英語の教師です。与えられた単語の語源を解説して。"
+      },
+      inputs: [":word"]
+    },
+    root: {
+      agent: "copyAgent",
+      isResult: true,
+      inputs: [":root_llm.choices.$0.message.content"]
     },
   },
 };
