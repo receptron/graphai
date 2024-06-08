f783ae2857c2569fb2144d0c9bc0ae31dcda6b6a
snakajima <satoshi.nakajima@gmail.com>
2024-06-08 08:01:46 -0700

    duration

--- packages/lite/src/index.ts
+++ packages/lite/src/index.ts
@@ -5,20 +5,22 @@ export const computed = async (nodes: any, func: any) => {
 
 export class Logger {
   public verbose: boolean;
+  public startTime: number;
   constructor(options: Record<string, any>) {
     this.verbose = options.verbose ?? false;
+    this.startTime = Date.now();
   }
 
   public async computed(nodes: any, func: any, options: Record<string, any> = { name: 'no name' }) {
     const results = await Promise.all(nodes);
-    if (this.verbose) {
-      console.log(`starting: ${options.name}`)
-    }
     const startTime = Date.now();
+    if (this.verbose) {
+      console.log(`starting: ${options.name} at ${startTime - this.startTime}`)
+    }
     const result = await func(...results);
     const endTime = Date.now();
     if (this.verbose) {
-      console.log(`complted: ${options.name}, ${endTime - startTime}ms`)
+      console.log(`complted: ${options.name} at ${endTime - this.startTime}, duration:${endTime - startTime}ms`)
     }
     return result;
   }
