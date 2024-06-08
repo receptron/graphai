381aae1dbdce716a96f23ee3f9c6906b33f87d22
snakajima <satoshi.nakajima@gmail.com>
2024-06-08 08:32:33 -0700

    version 0.0.8

--- packages/lite/lib/index.d.ts
+++ packages/lite/lib/index.d.ts
@@ -1,1 +1,9 @@
 export declare const computed: (nodes: any, func: any) => Promise<any>;
+export declare class Logger {
+    verbose: boolean;
+    startTime: number;
+    logs: Array<Record<string, any>>;
+    result: Record<string, any>;
+    constructor(options: Record<string, any>);
+    computed(nodes: any, func: any, options?: Record<string, any>): Promise<any>;
+}
--- packages/lite/lib/index.js
+++ packages/lite/lib/index.js
@@ -1,8 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.computed = void 0;
+exports.Logger = exports.computed = void 0;
 const computed = async (nodes, func) => {
     const results = await Promise.all(nodes);
     return func(...results);
 };
 exports.computed = computed;
+class Logger {
+    constructor(options) {
+        this.logs = [];
+        this.result = {};
+        this.verbose = options.verbose ?? false;
+        this.startTime = Date.now();
+    }
+    async computed(nodes, func, options = { name: 'no name' }) {
+        const results = await Promise.all(nodes);
+        const startTime = Date.now();
+        const logStart = {
+            name: options.name,
+            time: Date.now(),
+            state: "started",
+        };
+        this.logs.push(logStart);
+        if (this.verbose) {
+            console.log(`starting: ${logStart.name} at ${logStart.time - this.startTime}`);
+        }
+        const result = await func(...results);
+        const logEnd = {
+            name: options.name,
+            time: Date.now(),
+            state: "completed",
+        };
+        logEnd.duration = logEnd.time - startTime,
+            this.logs.push(logEnd);
+        if (this.verbose) {
+            console.log(`complted: ${logEnd.name} at ${logEnd.time - this.startTime}, duration:${logEnd.duration}ms`);
+        }
+        return result;
+    }
+}
+exports.Logger = Logger;
