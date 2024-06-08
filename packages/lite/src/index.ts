bf1dbd8dc15b0349493f159470f4590e40fc05fa
snakajima <satoshi.nakajima@gmail.com>
2024-06-08 08:08:24 -0700

    logs

--- packages/lite/src/index.ts
+++ packages/lite/src/index.ts
@@ -6,6 +6,7 @@ export const computed = async (nodes: any, func: any) => {
 export class Logger {
   public verbose: boolean;
   public startTime: number;
+  public logs: Array<Record<string, any>> = [];
   constructor(options: Record<string, any>) {
     this.verbose = options.verbose ?? false;
     this.startTime = Date.now();
@@ -14,13 +15,24 @@ export class Logger {
   public async computed(nodes: any, func: any, options: Record<string, any> = { name: 'no name' }) {
     const results = await Promise.all(nodes);
     const startTime = Date.now();
+    const logStart = {
+      name: options.name,
+      time: Date.now(),
+      state: "started",
+    };
+    this.logs.push(logStart);
     if (this.verbose) {
-      console.log(`starting: ${options.name} at ${startTime - this.startTime}`)
+      console.log(`starting: ${logStart.name} at ${logStart.time - this.startTime}`)
     }
     const result = await func(...results);
-    const endTime = Date.now();
+    const logEnd = {
+      name: options.name,
+      time: Date.now(),
+      state: "completed",
+    };
+    this.logs.push(logEnd);
     if (this.verbose) {
-      console.log(`complted: ${options.name} at ${endTime - this.startTime}, duration:${endTime - startTime}ms`)
+      console.log(`complted: ${logEnd.name} at ${logEnd.time - this.startTime}, duration:${logEnd.time - startTime}ms`)
     }
     return result;
   }
