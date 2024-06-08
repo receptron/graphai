e9b9e770078986be722151ad7e374d1a0fad2979
isamu <isamu@to-kyo.to>
2024-06-09 08:10:43 +0900

    move test_dynamic_graph

--- agents/vanilla_agents/tests/graph/test_dynamic_graph.ts
+++ agents/vanilla_agents/tests/graph/test_dynamic_graph.ts
@@ -1,5 +1,6 @@
 import { graphDataTestRunner } from "@graphai/test_utils";
 import * as agents from "@/index";
+import { sleeperAgent } from "@graphai/sleeper_agents";
 
 import test from "node:test";
 import assert from "node:assert";
@@ -44,7 +45,7 @@ const graphdata = {
 };
 
 test("test dynamic graph", async () => {
-  const result = await graphDataTestRunner(__dirname, __filename, graphdata, agents, () => {}, false);
+  const result = await graphDataTestRunner(__dirname, __filename, graphdata, { ...agents, sleeperAgent }, () => {}, false);
   assert.deepStrictEqual(result, {
     nested: {
       reducer: ["hello", "hello", "hello", "hello", "hello"],
@@ -71,7 +72,7 @@ const graphdata2 = {
 };
 
 test("test dynamic graph parser", async () => {
-  const result = await graphDataTestRunner(__dirname, __filename, graphdata2, agents, () => {}, false);
+  const result = await graphDataTestRunner(__dirname, __filename, graphdata2, { ...agents, sleeperAgent }, () => {}, false);
   assert.deepStrictEqual(result, {
     nested: {
       reducer: ["hello", "hello", "hello", "hello", "hello"],
@@ -98,7 +99,7 @@ const graphdata3 = {
 };
 
 test("test dynamic graph parser extra", async () => {
-  const result = await graphDataTestRunner(__dirname, __filename, graphdata3, agents, () => {}, false);
+  const result = await graphDataTestRunner(__dirname, __filename, graphdata3, { ...agents, sleeperAgent }, () => {}, false);
   assert.deepStrictEqual(result, {
     nested: {
       reducer: ["hello", "hello", "hello", "hello", "hello"],
