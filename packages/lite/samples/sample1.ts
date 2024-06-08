afb4601b09de15e3fa8bf64242d10e78de3ba04b
snakajima <satoshi.nakajima@gmail.com>
2024-06-08 09:05:00 -0700

    recordInputs and output

--- packages/lite/samples/sample1.ts
+++ packages/lite/samples/sample1.ts
@@ -65,7 +65,7 @@ const main = async () => {
   console.log(await timer(Answer3()));
   console.log(await timer(Answer6()));
   console.log(await timer(Answer8()));
-  const logger = new Logger({ verbose:true });
+  const logger = new Logger({ verbose:true, recordInputs: true, recordOutput: true });
   await Answer9(logger);
   console.log(logger.logs);
   console.log(logger.result);
--- packages/lite/src/index.ts
+++ packages/lite/src/index.ts
@@ -5,22 +5,29 @@ export const computed = async (nodes: any, func: any) => {
 
 export class Logger {
   public verbose: boolean;
+  public recordInputs: boolean;
+  public recordOutput: boolean;
   public startTime: number;
   public logs: Array<Record<string, any>> = [];
   public result: Record<string, any> = {};
   constructor(options: Record<string, any>) {
     this.verbose = options.verbose ?? false;
+    this.recordInputs = options.recordInputs ?? false;
+    this.recordOutput = options.recordOutput ?? false;
     this.startTime = Date.now();
   }
 
   public async computed(nodes: any, func: any, options: Record<string, any> = { name: "no name" }) {
     const inputs = await Promise.all(nodes);
     const startTime = Date.now();
-    const logStart = {
+    const logStart: any = {
       name: options.name,
       time: Date.now(),
       state: "started",
     };
+    if (this.recordInputs) {
+      logStart.inputs = inputs;
+    }
     this.logs.push(logStart);
     if (this.verbose) {
       console.log(`starting: ${logStart.name} at ${logStart.time - this.startTime}`);
@@ -31,7 +38,10 @@ export class Logger {
       time: Date.now(),
       state: "completed",
     };
-    logEnd.duration = logEnd.time - startTime,
+    logEnd.duration = logEnd.time - startTime;
+    if (this.recordOutput) {
+      logStart.outputs = output;
+    }
     this.logs.push(logEnd);
     if (this.verbose) {
       console.log(`complted: ${logEnd.name} at ${logEnd.time - this.startTime}, duration:${ logEnd.duration }ms`);
