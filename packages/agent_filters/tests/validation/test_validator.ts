332c0224691fd51437958ba67a56ba08aa468be4
isamu <isamu@to-kyo.to>
2024-06-10 19:59:10 +0900

    fix lint

--- packages/agent_filters/tests/validation/test_validator.ts
+++ packages/agent_filters/tests/validation/test_validator.ts
@@ -2,12 +2,12 @@ import * as agents from "@graphai/agents";
 import { agentInputValidator } from "@/index";
 
 import test from "node:test";
-import assert from "node:assert";
+// import assert from "node:assert";
 
 test("test validate", async () => {
   for (const agentInfo of Object.values(agents)) {
     if (agentInfo?.inputs && agentInfo?.samples) {
-      for (const sample of agentInfo?.samples) {
+      for (const sample of agentInfo.samples) {
         try {
           agentInputValidator(agentInfo?.inputs, sample.inputs);
         } catch (e) {
