af6ae59c30887e0091d54602c7a0df51c2e548b0
isamu <isamu@to-kyo.to>
2024-06-11 05:23:19 +0900

    remove agents/agents

--- packages/agents/lib/agents.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "@graphai/input_agents";
-export * from "@graphai/data_agents";
-export * from "@graphai/llm_agents";
-export * from "@graphai/service_agents";
-export * from "@graphai/sleeper_agents";
