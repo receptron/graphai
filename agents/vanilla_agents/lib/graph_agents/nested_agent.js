74023a3ea2c89b66f7b6dccd5a7ddda12ed9dfa3
snakajima <satoshi.nakajima@gmail.com>
2024-06-11 14:58:41 -0700

    built

--- agents/vanilla_agents/lib/graph_agents/nested_agent.js
+++ agents/vanilla_agents/lib/graph_agents/nested_agent.js
@@ -1,64 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.nestedAgent = exports.getNestedGraphData = void 0;
+exports.nestedAgent = void 0;
 const graphai_1 = require("graphai");
-// This function allows us to use one of inputs as the graph data for this nested agent,
-// which is equivalent to "eval" of JavaScript.
-const getNestedGraphData = (graphData, __inputs) => {
-    (0, graphai_1.assert)(graphData !== undefined, "nestedAgent: graphData is required");
-    if (typeof graphData === "string") {
-        // We no longer need this feature bacause graph can have a data source
-        /*
-        const regex = /^\$(\d+)$/;
-        const match = graphData.match(regex);
-        if (match) {
-    
-          const index = parseInt(match[1], 10);
-          if (index < inputs.length) {
-            return inputs[index] as GraphData;
-          }
-        }
-        */
-        (0, graphai_1.assert)(false, `getNestedGraphData: Invalid graphData string: ${graphData}`);
-    }
-    return graphData;
-};
-exports.getNestedGraphData = getNestedGraphData;
-const nestedAgent = async ({ params, inputs, namedInputs, agents, log, taskManager, graphData, agentFilters, debugInfo }) => {
+const nestedAgent = async ({ namedInputs, agents, log, taskManager, graphData, agentFilters, debugInfo }) => {
     if (taskManager) {
         const status = taskManager.getStatus(false);
         (0, graphai_1.assert)(status.concurrency > status.running, `nestedAgent: Concurrency is too low: ${status.concurrency}`);
     }
-    const nestedGraphData = (0, exports.getNestedGraphData)(graphData, inputs);
-    if (inputs.length > 0) {
-        // LATER: Remove this old way
-        console.log("-------------------- inputs for nestedGraph");
-        const namedInputs = params.namedInputs ?? inputs.map((__input, index) => `$${index}`);
-        namedInputs.forEach((nodeId, index) => {
+    (0, graphai_1.assert)(!!graphData, "nestedAgent: graph is required");
+    const { nodes } = graphData;
+    const nestedGraphData = { ...graphData, nodes: { ...nodes } }; // deep enough copy
+    const nodeIds = Object.keys(namedInputs);
+    if (nodeIds.length > 0) {
+        nodeIds.forEach((nodeId) => {
             if (nestedGraphData.nodes[nodeId] === undefined) {
                 // If the input node does not exist, automatically create a static node
-                nestedGraphData.nodes[nodeId] = { value: inputs[index] };
+                nestedGraphData.nodes[nodeId] = { value: namedInputs[nodeId] };
             }
             else {
                 // Otherwise, inject the proper data here (instead of calling injectTo method later)
-                nestedGraphData.nodes[nodeId]["value"] = inputs[index];
+                nestedGraphData.nodes[nodeId]["value"] = namedInputs[nodeId];
             }
         });
-    }
-    else {
-        const nodeIds = Object.keys(namedInputs);
-        if (nodeIds.length > 0) {
-            nodeIds.forEach((nodeId) => {
-                if (nestedGraphData.nodes[nodeId] === undefined) {
-                    // If the input node does not exist, automatically create a static node
-                    nestedGraphData.nodes[nodeId] = { value: namedInputs[nodeId] };
-                }
-                else {
-                    // Otherwise, inject the proper data here (instead of calling injectTo method later)
-                    nestedGraphData.nodes[nodeId]["value"] = namedInputs[nodeId];
-                }
-            });
-        }
     }
     try {
         if (nestedGraphData.version === undefined && debugInfo.version) {
