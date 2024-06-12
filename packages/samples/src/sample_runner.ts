fc7ce2ca60d489ecff2ddcf2e2f7e0efdd2471ab
isamu <isamu@to-kyo.to>
2024-06-12 13:12:34 +0900

    update sample runner

--- packages/samples/src/sample_runner.ts
+++ packages/samples/src/sample_runner.ts
@@ -5,41 +5,47 @@ import { main as streaming_openai } from "./streaming/openai";
 import { main as streaming_slashgpt } from "./streaming/slashgpt";
 import { main as streaming_openai_agent } from "./streaming/openai_agent";
 
-import { main as benchmark } from "./benchmarks/benchmark";
-import { main as benchmark2 } from "./benchmarks/benchmark2";
+import { main as benchmark } from "../../../agents/llm_agents/samples/benchmarks/benchmark";
+import { main as benchmark2 } from "../../../agents/llm_agents/samples/benchmarks/benchmark2";
 
-import { main as wikipedia } from "./embeddings/wikipedia";
+// import { main as wikipedia } from "../../../agents/llm_agents/samples/embeddings/wikipedia";
 
 import { main as test_fibonacci } from "./test/fibonacci";
 import { main as test_loop } from "./test/loop";
 
 import { main as slashgpt } from "./llm/slashgpt";
-import { main as groq } from "./llm/groq";
-import { main as claude } from "./llm/claude";
-import { main as gpt4o } from "./llm/gpt4o";
-import { main as interview_jp } from "./llm/interview_jp";
-import { main as interview } from "./llm/interview";
-import { main as interview_ollama } from "./llm/interview_ollama";
-import { main as interview_phi3 } from "./llm/interview_phi3";
-import { main as research } from "./llm/review";
-import { main as rewrite } from "./llm/rewrite";
-import { main as describe_graph } from "./llm/describe_graph";
-import { main as gemini } from "./llm/gemini";
+/*
+import { main as groq } from "../../../agents/llm_agents/samples/llm/groq";
+import { main as claude } from "../../../agents/llm_agents/samples/llm/claude";
+import { main as gpt4o } from "../../../agents/llm_agents/samples/llm/gpt4o";
+import { main as interview_jp } from "../../../agents/llm_agents/samples/llm/interview_jp";
+import { main as interview } from "../../../agents/llm_agents/samples/llm/interview";
+import { main as interview_ollama } from "../../../agents/llm_agents/samples/llm/interview_ollama";
+import { main as interview_phi3 } from "../../../agents/llm_agents/samples/llm/interview_phi3";
+import { main as research } from "../../../agents/llm_agents/samples/llm/review";
+import { main as rewrite } from "../../../agents/llm_agents/samples/llm/rewrite";
+import { main as describe_graph } from "../../../agents/llm_agents/samples/llm/describe_graph";
+import { main as gemini } from "../../../agents/llm_agents/samples/llm/gemini";
+*/
 
 import { main as net_paper_ai } from "./net/paper_ai";
-import { main as net_rss } from "./net/rss";
-import { main as weather } from "./net/weather";
 
-import { main as interaction_text } from "./interaction/text";
+/*
+import { main as net_rss } from "../../../agents/llm_agents/samples/net/rss";
+import { main as weather } from "../../../agents/llm_agents/samples/net/weather";
+*/
+
 import { main as interaction_select } from "./interaction/select";
-import { main as interaction_chat } from "./interaction/chat";
-import { main as interaction_chat_gemini } from "./interaction/chat_gemini";
-import { main as interaction_metachat } from "./interaction/metachat";
-import { main as interaction_metachat_gemini } from "./interaction/metachat_gemini";
-import { main as interaction_reception } from "./interaction/reception";
-import { main as interaction_reception_gemini } from "./interaction/reception_gemini";
-import { main as interaction_wikipedia } from "./interaction/wikipedia";
-
+/*
+import { main as interaction_text } from "../../../agents/llm_agents/samples/interaction/text";
+import { main as interaction_chat } from "../../../agents/llm_agents/samples/interaction/chat";
+import { main as interaction_chat_gemini } from "../../../agents/llm_agents/samples/interaction/chat_gemini";
+import { main as interaction_metachat } from "../../../agents/llm_agents/samples/interaction/metachat";
+import { main as interaction_metachat_gemini } from "../../../agents/llm_agents/samples/interaction/metachat_gemini";
+import { main as interaction_reception } from "../../../agents/llm_agents/samples/interaction/reception";
+import { main as interaction_reception_gemini } from "../../../agents/llm_agents/samples/interaction/reception_gemini";
+import { main as interaction_wikipedia } from "../../../agents/llm_agents/samples/interaction/wikipedia";
+*/
 import { main as sample_co2 } from "./tools/sample_co2";
 import { main as tools_home } from "./tools/home";
 import { main as tools_grop } from "./tools/groq";
@@ -54,7 +60,7 @@ const main = async () => {
   await benchmark();
   await benchmark2();
 
-  await wikipedia();
+//  await wikipedia();
 
   // test
   await test_fibonacci();
@@ -62,6 +68,7 @@ const main = async () => {
 
   // llm
   await slashgpt();
+/*
   await groq();
   await claude();
   await gpt4o();
@@ -73,10 +80,11 @@ const main = async () => {
   await rewrite();
   await describe_graph();
   await gemini();
-
+*/
   // interaction
-  await interaction_text();
   await interaction_select();
+/*  
+  await interaction_text();
   await interaction_chat();
   await interaction_chat_gemini();
   await interaction_metachat();
@@ -84,11 +92,12 @@ const main = async () => {
   await interaction_reception();
   await interaction_reception_gemini();
   await interaction_wikipedia();
-
+*/
+  
   // net
   await net_paper_ai();
-  await net_rss();
-  await weather();
+  //  await net_rss();
+  //  await weather();
 
   // tools
   await sample_co2();
