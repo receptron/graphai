6aeb9db2793b23d79102daffc97ce59991c766e2
isamu <isamu@to-kyo.to>
2024-06-12 17:03:44 +0900

    remove vanilla.ts

--- agents/vanilla_agents/src/data_agents/index.ts
+++ agents/vanilla_agents/src/data_agents/index.ts
@@ -1,1 +1,6 @@
-export * from "./vanilla";
+import totalAgent from "./total_agent";
+import dataSumTemplateAgent from "./data_sum_template_agent";
+import propertyFilterAgent from "./property_filter_agent";
+import copyAgent from "./copy_agent";
+
+export { totalAgent, dataSumTemplateAgent, propertyFilterAgent, copyAgent };
