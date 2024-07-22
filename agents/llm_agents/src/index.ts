2227dd9ab8381adc2001d70982d5635b42ab3870
isamu <isamu@to-kyo.to>
2024-07-23 05:58:42 +0900

    slashgpt

--- agents/llm_agents/src/index.ts
+++ agents/llm_agents/src/index.ts
@@ -1,9 +1,9 @@
 import groqAgent from "./groq_agent";
-import slashGPTAgent from "./slashgpt_agent";
 import anthropicAgent from "./anthropic_agent";
 import geminiAgent from "./gemini_agent";
 import tokenBoundStringsAgent from "./token_bound_string_agent";
 
 import { openAIAgent } from "@graphai/openai_agent";
+import { slashGPTAgent } from "@graphai/slashgpt_agent";
 
 export { groqAgent, slashGPTAgent, openAIAgent, anthropicAgent, geminiAgent, tokenBoundStringsAgent };
