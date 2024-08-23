b61a1709996cfeba63828ca6ff3b3442d0527ba6
isamu <isamu@to-kyo.to>
2024-08-23 12:14:10 +0900

    build

--- packages/cli/lib/agentdoc.js
+++ packages/cli/lib/agentdoc.js
@@ -1,5 +1,28 @@
 #!/usr/bin/env node
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,8 +32,7 @@ const docs_1 = require("./docs");
 const main = async () => {
     const path = process.cwd();
     const packageJson = JSON.parse(fs_1.default.readFileSync(path + "/package.json", "utf8"));
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const agents = require(path + "/lib/index");
+    const agents = await Promise.resolve(`${path + "/lib/index"}`).then(s => __importStar(require(s)));
     const agentAttribute = (key) => {
         if (key === "packageName") {
             return packageJson.name;
