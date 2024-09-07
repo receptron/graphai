cb0aa59d031f08accb56ac0564bbbafddc558955
isamu <isamu@to-kyo.to>
2024-09-07 17:35:08 +0900

    move map test

--- agents/vanilla_agents/tests/graph/test_map.ts
+++ agents/vanilla_agents/tests/graph/test_map.ts
@@ -1,11 +1,81 @@
 import { graphDataTestRunner, fileTestRunner } from "@receptron/test_utils";
-import * as agents from "@/index";
+import * as vanilla_agents from "@/index";
+import { sleeperAgent } from "@graphai/sleeper_agents";
+const agents = {
+  sleeperAgent,
+  ...vanilla_agents,
+};
 
 import test from "node:test";
 import assert from "node:assert";
 
+const graphdata_map = {
+  version: 0.5,
+  nodes: {
+    source: {
+      value: {
+        fruits: ["apple", "orange", "banana", "lemon", "melon", "pineapple", "tomato"],
+      },
+    },
+    nestedNode: {
+      agent: "mapAgent",
+      inputs: {
+        rows: ":source.fruits",
+      },
+      graph: {
+        version: 0.5,
+        nodes: {
+          node2: {
+            agent: "stringTemplateAgent",
+            params: {
+              template: "I love ${0}.",
+            },
+            inputs: [":row"],
+            isResult: true,
+          },
+        },
+      },
+    },
+    result: {
+      agent: "sleeperAgent",
+      inputs: [":nestedNode.node2"],
+      isResult: true,
+    },
+  },
+};
+
+const graphdata_map3 = {
+  version: 0.5,
+  nodes: {
+    source1: {
+      value: ["hello", "hello2"],
+    },
+    nestedNode: {
+      agent: "mapAgent",
+      inputs: {
+        rows: ":source1",
+      },
+      graph: {
+        version: 0.5,
+        nodes: {
+          node1: {
+            agent: "bypassAgent",
+            inputs: [":row"],
+            isResult: true,
+          },
+        },
+      },
+    },
+    result: {
+      agent: "bypassAgent",
+      inputs: [":nestedNode.node1"],
+      isResult: true,
+    },
+  },
+};
+
 test("test map 1", async () => {
-  const result = await fileTestRunner(__dirname, "/graphs/map/map1.yaml", agents);
+  const result = await graphDataTestRunner(__dirname, "test_map1", graphdata_map, agents);
   assert.deepStrictEqual(result.result, [
     "I love apple.",
     "I love orange.",
@@ -17,16 +87,9 @@ test("test map 1", async () => {
   ]);
 });
 
-/* Removed for now (named Inputs)
-test("test map 2", async () => {
-  const result = await fileTestRunner(__dirname, "/graphs/map/map2.yaml", agents);
-  assert.deepStrictEqual(result.result, ["I love apple.", "I love orange.", "I love banana.", "I love lemon."]);
-});
-*/
-
 // nest graph and flat
 test("test map 3", async () => {
-  const result = await fileTestRunner(__dirname, "/graphs/map/map3.yaml", agents);
+  const result = await graphDataTestRunner(__dirname, "test_map3", graphdata_map3, agents);
   assert.deepStrictEqual(result.result, [[["hello"], ["hello2"]]]);
 });
 
