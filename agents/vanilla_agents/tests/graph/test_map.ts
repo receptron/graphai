c10ccc81054a6feeb4bfc0978405a8d96adf65f4
isamu <isamu@to-kyo.to>
2024-09-07 17:40:07 +0900

    map examples

--- agents/vanilla_agents/tests/graph/test_map.ts
+++ agents/vanilla_agents/tests/graph/test_map.ts
@@ -6,76 +6,13 @@ const agents = {
   ...vanilla_agents,
 };
 
+import { graphdataMap1, graphdataMap3, graphdataMap4, graphdataMap5 } from "./graphData";
+
 import test from "node:test";
 import assert from "node:assert";
 
-const graphdata_map = {
-  version: 0.5,
-  nodes: {
-    source: {
-      value: {
-        fruits: ["apple", "orange", "banana", "lemon", "melon", "pineapple", "tomato"],
-      },
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: {
-        rows: ":source.fruits",
-      },
-      graph: {
-        version: 0.5,
-        nodes: {
-          node2: {
-            agent: "stringTemplateAgent",
-            params: {
-              template: "I love ${0}.",
-            },
-            inputs: [":row"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "sleeperAgent",
-      inputs: [":nestedNode.node2"],
-      isResult: true,
-    },
-  },
-};
-
-const graphdata_map3 = {
-  version: 0.5,
-  nodes: {
-    source1: {
-      value: ["hello", "hello2"],
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: {
-        rows: ":source1",
-      },
-      graph: {
-        version: 0.5,
-        nodes: {
-          node1: {
-            agent: "bypassAgent",
-            inputs: [":row"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "bypassAgent",
-      inputs: [":nestedNode.node1"],
-      isResult: true,
-    },
-  },
-};
-
 test("test map 1", async () => {
-  const result = await graphDataTestRunner(__dirname, "test_map1", graphdata_map, agents);
+  const result = await graphDataTestRunner(__dirname, "test_map1", graphdataMap1, agents);
   assert.deepStrictEqual(result.result, [
     "I love apple.",
     "I love orange.",
@@ -89,82 +26,18 @@ test("test map 1", async () => {
 
 // nest graph and flat
 test("test map 3", async () => {
-  const result = await graphDataTestRunner(__dirname, "test_map3", graphdata_map3, agents);
+  const result = await graphDataTestRunner(__dirname, "test_map3", graphdataMap3, agents);
   assert.deepStrictEqual(result.result, [[["hello"], ["hello2"]]]);
 });
 
-const graphdata_map4 = {
-  version: 0.5,
-  nodes: {
-    source1: {
-      value: ["hello", "hello2"],
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: {
-        rows: ":source1",
-      },
-      graph: {
-        version: 0.5,
-        nodes: {
-          node1: {
-            agent: "bypassAgent",
-            inputs: [":row"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "bypassAgent",
-      params: {
-        flat: 1,
-      },
-      inputs: [":nestedNode.node1"],
-    },
-  },
-};
-
 test("test map 4", async () => {
-  const result = await graphDataTestRunner(__dirname, "test_map4", graphdata_map4, agents);
+  const result = await graphDataTestRunner(__dirname, "test_map4", graphdataMap4, agents);
 
   assert.deepStrictEqual(result.result, [["hello"], ["hello2"]]);
 });
 
-const graphdata_map5 = {
-  version: 0.5,
-  nodes: {
-    source1: {
-      value: ["hello", "hello2"],
-    },
-    nestedNode: {
-      agent: "mapAgent",
-      inputs: {
-        rows: ":source1",
-      },
-      graph: {
-        version: 0.5,
-        nodes: {
-          node1: {
-            agent: "bypassAgent",
-            inputs: [":row"],
-            isResult: true,
-          },
-        },
-      },
-    },
-    result: {
-      agent: "bypassAgent",
-      params: {
-        flat: 2,
-      },
-      inputs: [":nestedNode.node1"],
-    },
-  },
-};
-
 test("test map 5", async () => {
-  const result = await graphDataTestRunner(__dirname, "test_map5", graphdata_map5, agents);
+  const result = await graphDataTestRunner(__dirname, "test_map5", graphdataMap5, agents);
 
   assert.deepStrictEqual(result.result, ["hello", "hello2"]);
 });
