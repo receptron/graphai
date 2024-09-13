c6cc2abad63da9082e2f42ed3d87447e6d22fcbe
isamu <isamu@to-kyo.to>
2024-09-13 16:39:01 +0900

    add sample2GraphData

---
+++ agents/agent_utils/src/index.ts
@@ -0,0 +1,35 @@
+import { AgentFunctionInfoSample, GraphData, NodeData } from "graphai";
+
+export const sample2GraphData = (sample: AgentFunctionInfoSample, agentName: string) => {
+  const nodes: Record<string, NodeData> = {};
+  const inputs = (() => {
+    if (Array.isArray(sample.inputs)) {
+      Array.from(sample.inputs.keys()).forEach((key) => {
+        nodes["sampleInput" + key] = {
+          value: sample.inputs[key],
+        };
+      });
+      return Object.keys(nodes).map((k) => ":" + k);
+    }
+    nodes["sampleInput"] = {
+      value: sample.inputs,
+    };
+    return Object.keys(sample.inputs).reduce((tmp: Record<string, string>, key: string) => {
+      tmp[key] = `:sampleInput.` + key;
+      return tmp;
+    }, {});
+  })();
+
+  nodes["node"] = {
+    isResult: true,
+    agent: agentName,
+    params: sample.params,
+    inputs: inputs,
+  };
+  const graphData: GraphData = {
+    version: 0.5,
+    nodes,
+  };
+  return graphData;
+};
+
