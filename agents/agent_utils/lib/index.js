8b68b8366a220b31be4caa31f62e163239fcd393
isamu <isamu@to-kyo.to>
2024-09-13 21:37:18 +0900

    add build

---
+++ agents/agent_utils/lib/index.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sample2GraphData = void 0;
+const sample2GraphData = (sample, agentName) => {
+    const nodes = {};
+    const inputs = (() => {
+        if (Array.isArray(sample.inputs)) {
+            Array.from(sample.inputs.keys()).forEach((key) => {
+                nodes["sampleInput" + key] = {
+                    value: sample.inputs[key],
+                };
+            });
+            return Object.keys(nodes).map((k) => ":" + k);
+        }
+        nodes["sampleInput"] = {
+            value: sample.inputs,
+        };
+        return Object.keys(sample.inputs).reduce((tmp, key) => {
+            tmp[key] = `:sampleInput.` + key;
+            return tmp;
+        }, {});
+    })();
+    nodes["node"] = {
+        isResult: true,
+        agent: agentName,
+        params: sample.params,
+        inputs: inputs,
+    };
+    const graphData = {
+        version: 0.5,
+        nodes,
+    };
+    return graphData;
+};
+exports.sample2GraphData = sample2GraphData;
