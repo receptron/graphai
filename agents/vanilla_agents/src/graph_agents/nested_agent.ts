8c578353cacf140d8ee7bffd6c2ac909298b361a
isamu <isamu@to-kyo.to>
2024-09-14 16:45:54 +0900

    add graphDataLatestVersion to nested agent

--- agents/vanilla_agents/src/graph_agents/nested_agent.ts
+++ agents/vanilla_agents/src/graph_agents/nested_agent.ts
@@ -1,4 +1,4 @@
-import { GraphAI, AgentFunction, AgentFunctionInfo, StaticNodeData, assert } from "graphai";
+import { GraphAI, AgentFunction, AgentFunctionInfo, StaticNodeData, assert, graphDataLatestVersion } from "graphai";
 
 export const nestedAgent: AgentFunction<{
   namedInputs?: Array<string>;
@@ -10,7 +10,7 @@ export const nestedAgent: AgentFunction<{
   assert(!!graphData, "nestedAgent: graph is required");
 
   const { nodes } = graphData;
-  const nestedGraphData = { ...graphData, nodes: { ...nodes } }; // deep enough copy
+  const nestedGraphData = { ...graphData, nodes: { ...nodes }, version: graphDataLatestVersion }; // deep enough copy
 
   const nodeIds = Object.keys(namedInputs);
   if (nodeIds.length > 0) {
