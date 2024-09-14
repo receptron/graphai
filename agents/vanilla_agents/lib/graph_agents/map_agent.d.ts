a6b0840d29f739b4147d9eeb72b9dfc56e18876e
isamu <isamu@to-kyo.to>
2024-09-15 05:04:01 +0900

    build

--- agents/vanilla_agents/lib/graph_agents/map_agent.d.ts
+++ agents/vanilla_agents/lib/graph_agents/map_agent.d.ts
@@ -2,6 +2,7 @@ import { AgentFunction, AgentFunctionInfo } from "graphai";
 export declare const mapAgent: AgentFunction<{
     limit?: number;
     resultAll?: boolean;
+    compositeResult?: boolean;
 }, Record<string, any>, any>;
 declare const mapAgentInfo: AgentFunctionInfo;
 export default mapAgentInfo;
