a0457637611377d92f10b9f9ad1a62b97a01cc0c
isamu <isamu@to-kyo.to>
2024-09-22 11:35:27 +0900

    merge node id agent support named inputs

--- agents/vanilla_agents/src/test_agents/merge_node_id_agent.ts
+++ agents/vanilla_agents/src/test_agents/merge_node_id_agent.ts
@@ -1,8 +1,15 @@
 import { AgentFunction, AgentFunctionInfo } from "graphai";
+import { isNamedInputs } from "@graphai/agent_utils";
 
-export const mergeNodeIdAgent: AgentFunction = async ({ debugInfo: { nodeId }, inputs }) => {
+export const mergeNodeIdAgent: AgentFunction<null, Record<string, unknown>, Record<string, unknown>, { array: Record<string, unknown>[] }> = async ({
+  debugInfo: { nodeId },
+  inputs,
+  namedInputs,
+}) => {
   // console.log("executing", nodeId);
-  return inputs.reduce(
+  const dataSet = isNamedInputs(namedInputs) ? namedInputs.array : inputs;
+
+  return dataSet.reduce(
     (tmp, input) => {
       return { ...tmp, ...input };
     },
