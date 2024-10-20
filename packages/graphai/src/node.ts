7a6577fe9bc379d4ba327be01203cd08ea0ad64a
isamu <isamu@to-kyo.to>
2024-10-20 11:21:16 +0900

    isNamedInputs

--- packages/graphai/src/node.ts
+++ packages/graphai/src/node.ts
@@ -96,12 +96,12 @@ export class ComputedNode extends Node {
 
     this.anyInput = data.anyInput ?? false;
     if (data.inputs) {
+      this.isNamedInputs = !Array.isArray(data.inputs);
       if (Array.isArray(data.inputs)) {
         console.warn(`array inputs have been deprecated. nodeId: ${nodeId}: see https://github.com/receptron/graphai/blob/main/docs/NamedInputs.md`);
         this.inputs = data.inputs;
         this.dataSources = inputs2dataSources(data.inputs, graph.version);
       } else {
-        this.isNamedInputs = true;
         this.dataSources = namedInputs2dataSources(data.inputs, graph.version);
       }
     }
