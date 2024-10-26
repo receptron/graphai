18b622a319aff6dd064bdcb26842957199ba8a11
isamu <isamu@to-kyo.to>
2024-10-26 19:34:43 +0900

    add build

---
+++ agents/sleeper_agents/lib/sleep_and_merge_agent.d.ts
@@ -0,0 +1,7 @@
+import { AgentFunction, AgentFunctionInfo } from "graphai";
+export declare const sleepAndMergeAgent: AgentFunction<{
+    duration?: number;
+    value?: Record<string, any>;
+}>;
+declare const sleeperAndMergeInfo: AgentFunctionInfo;
+export default sleeperAndMergeInfo;
