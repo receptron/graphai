6156d639cf6ce5f1d8881f3de4f0f00b1c9fe093
isamu <isamu@to-kyo.to>
2024-11-08 10:29:04 +0900

    add graphLoader map sample

---
+++ packages/samples/src/graphLoader/map.ts
@@ -0,0 +1,30 @@
+import { GraphAI, GraphDataLoaderOption, GraphData } from "graphai";
+import * as agents from "@graphai/vanilla";
+
+import { parse } from "yaml";
+import { readFileSync } from "fs";
+
+const readYaml = (fileName: string) => {
+  const path = __dirname + "/" + fileName;
+  const yamlStr = readFileSync(path, "utf8");
+  const ret = parse(yamlStr);
+  return ret;
+};
+
+export const main = async () => {
+  const graphLoader = (args: GraphDataLoaderOption) => {
+    const { fileName, option } = args;
+    const graphData = readYaml(fileName);
+    if (option && option.setting) {
+      const settings = readYaml(option.setting);
+      graphData.nodes["data"] = { value: settings.data };
+    }
+    return graphData as GraphData;
+  };
+  const baseGraph = readYaml("mapParent.yaml");
+  const graph = new GraphAI(baseGraph, agents, { graphLoader });
+  const result = await graph.run(true);
+  console.log(result);
+};
+
+main();
