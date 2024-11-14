63b3426b90a68f73936eb7a23386963d81b5f13b
isamu <isamu@to-kyo.to>
2024-11-14 09:58:52 +0900

    format

--- agents/vanilla_agents/rollup.config.mjs
+++ agents/vanilla_agents/rollup.config.mjs
@@ -1,47 +1,38 @@
 import pluginTypescript from "@rollup/plugin-typescript";
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 
-export default [{
-  input: './src/index.ts',
-  output: {
-    file: './lib/bundle.min.mjs',
-    format: 'esm',
-    sourcemap: true,
-  },
-  external: ['graphai'],
+export default [
+  {
+    input: "./src/index.ts",
+    output: {
+      file: "./lib/bundle.min.mjs",
+      format: "esm",
+      sourcemap: true,
+    },
+    external: ["graphai"],
 
-  plugins: [
-    resolve(),
-    commonjs(),
-    pluginTypescript(),
-    terser(),
-  ],
-},{
-  input: './src/index.ts',
-  output: {
-    file: './lib/bundle.mjs',
-    format: 'esm',
-    sourcemap: true,
+    plugins: [resolve(), commonjs(), pluginTypescript(), terser()],
   },
-  external: ['graphai'],
-  plugins: [
-    resolve(),
-    commonjs(),
-    pluginTypescript(),
-  ],
-},{
-  input: './src/index.ts',
-  output: {
-    name: 'vanilla_agents',
-    file: './lib/bundle.umd.mjs',
-    format: 'umd',
-    sourcemap: true,
+  {
+    input: "./src/index.ts",
+    output: {
+      file: "./lib/bundle.mjs",
+      format: "esm",
+      sourcemap: true,
+    },
+    external: ["graphai"],
+    plugins: [resolve(), commonjs(), pluginTypescript()],
   },
-  plugins: [
-    resolve(),
-    commonjs(),
-    pluginTypescript(),
-  ],
-}];
+  {
+    input: "./src/index.ts",
+    output: {
+      name: "vanilla_agents",
+      file: "./lib/bundle.umd.mjs",
+      format: "umd",
+      sourcemap: true,
+    },
+    plugins: [resolve(), commonjs(), pluginTypescript()],
+  },
+];
