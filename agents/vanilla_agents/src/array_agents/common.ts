9306400676435f3abf3f6ee00414a3931f491af7
isamu <isamu@to-kyo.to>
2024-11-22 07:42:01 +0900

    array validate

--- agents/vanilla_agents/src/array_agents/common.ts
+++ agents/vanilla_agents/src/array_agents/common.ts
@@ -1,9 +1,8 @@
 import { assert } from "graphai";
 import { isNamedInputs } from "@graphai/agent_utils";
 
-export const arrayValidate = (agentName: string, namedInputs: { array: Array<unknown> }) => {
-  assert(!!namedInputs, `${agentName}: namedInputs is UNDEFINED!`);
-  assert(isNamedInputs(namedInputs), "popAgent: namedInputs is UNDEFINED!");
-  assert(!!namedInputs.array, `${agentName}: namedInputs.array is UNDEFINED!`);
-  assert(Array.isArray(namedInputs.array), `${agentName}: namedInputs.array is not Array`);
+export const arrayValidate = (agentName: string, namedInputs: { array: Array<unknown> }, extra_message: string = "") => {
+  assert(isNamedInputs(namedInputs), `${agentName}: namedInputs is UNDEFINED!` + extra_message);
+  assert(!!namedInputs.array, `${agentName}: namedInputs.array is UNDEFINED!` + extra_message);
+  assert(Array.isArray(namedInputs.array), `${agentName}: namedInputs.array is not Array.` + extra_message);
 };
