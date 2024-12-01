090b2378c0509c28261326c937755f2d4f5d3459
isamu <isamu@to-kyo.to>
2024-12-01 12:40:14 +0900

    add images2messageAgent

---
+++ agents/vanilla_agents/src/images_agents/index.ts
@@ -0,0 +1,5 @@
+import images2messageAgent from "./image_to_message_agent";
+// import imageReadAgent from "./image_read_agent";
+
+// export { images2messageAgent, imageReadAgent };
+export { images2messageAgent };
