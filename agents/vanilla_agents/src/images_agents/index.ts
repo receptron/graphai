edc40e73c577601bd57398c6e97e01f70ee77927
isamu <isamu@to-kyo.to>
2024-12-01 13:32:06 +0900

    cleanup import

--- agents/vanilla_agents/src/images_agents/index.ts
+++ agents/vanilla_agents/src/images_agents/index.ts
@@ -1,5 +1,2 @@
 import images2messageAgent from "./image_to_message_agent";
-// import imageReadAgent from "./image_read_agent";
-
-// export { images2messageAgent, imageReadAgent };
 export { images2messageAgent };
