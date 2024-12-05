bcc4404361a4825e1c6780e56062dc5e1024e1a6
isamu <isamu@to-kyo.to>
2024-12-06 08:05:56 +0900

    update

--- agents/vanilla_node_agents/lib/node_file_agents/index.js
+++ agents/vanilla_node_agents/lib/node_file_agents/index.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileReadAgent = void 0;
+exports.fileWriteAgent = exports.fileReadAgent = void 0;
 const file_read_agent_1 = __importDefault(require("./file_read_agent"));
 exports.fileReadAgent = file_read_agent_1.default;
+const file_write_agent_1 = __importDefault(require("./file_write_agent"));
+exports.fileWriteAgent = file_write_agent_1.default;
