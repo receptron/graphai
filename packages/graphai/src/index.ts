0484f4264a5c6a3353b5aea85e341710908b055f
isamu <isamu@to-kyo.to>
2024-12-18 07:19:23 +0900

    update

--- packages/graphai/src/index.ts
+++ packages/graphai/src/index.ts
@@ -24,7 +24,7 @@ export {
 
 export type { TransactionLog } from "./transaction_log";
 
-export { defaultAgentInfo, agentInfoWrapper, defaultTestContext, strIntentionalError, assert, sleep, isObject, parseNodeName } from "./utils/utils";
+export { defaultAgentInfo, agentInfoWrapper, defaultTestContext, strIntentionalError, assert, sleep, isObject, parseNodeName, debugResultKey } from "./utils/utils";
 export { inputs2dataSources } from "./utils/nodeUtils";
 
 export { ValidationError } from "./validators/common";
