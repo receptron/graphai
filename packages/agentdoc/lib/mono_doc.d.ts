0207983f53ca50db6ba902a548f13a6dd72c48bf
isamu <isamu@to-kyo.to>
2024-12-18 07:20:47 +0900

    update monoDoc

---
+++ packages/agentdoc/lib/mono_doc.d.ts
@@ -0,0 +1,3 @@
+import { AgentFunctionInfoDictionary } from "graphai";
+export declare const readTemplate: (file: string) => string;
+export declare const generateMonoDoc: (base_path: string, agents: AgentFunctionInfoDictionary) => void;
