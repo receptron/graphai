664bcf1583721cdf60ecd41c5cadccf33ee0d391
isamu <isamu@to-kyo.to>
2024-12-18 06:41:00 +0900

    refactorAgentDoc 0.0.7

--- packages/agentdoc/lib/agentdoc.d.ts
+++ packages/agentdoc/lib/agentdoc.d.ts
@@ -1,1 +1,7 @@
+import { PackageJson } from "type-fest";
+export declare const getAgents: (agentKeys: string[]) => string | undefined;
+export declare const getGitRep: (repository: string | {
+    url: string;
+}) => string;
+export declare const getPackageJson: (npmRootPath: string) => PackageJson | null;
 export declare const main: (npmRootPath: string) => Promise<void>;
