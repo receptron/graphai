edaef6a434a143ec9d85c0e3530e8e38802bcb03
isamu <isamu@to-kyo.to>
2024-12-25 15:47:21 +0900

    agentId

--- packages/graphai/src/node.ts
+++ packages/graphai/src/node.ts
@@ -233,9 +233,9 @@ export class ComputedNode extends Node {
   }
 
   // Check if we need to apply this filter to this node or not.
-  private shouldApplyAgentFilter(agentFilter: AgentFilterInfo) {
+  private shouldApplyAgentFilter(agentFilter: AgentFilterInfo, agentId?: string) {
     if (agentFilter.agentIds && Array.isArray(agentFilter.agentIds) && agentFilter.agentIds.length > 0) {
-      if (this.agentId && agentFilter.agentIds.includes(this.agentId)) {
+      if (agentId && agentFilter.agentIds.includes(agentId)) {
         return true;
       }
     }
@@ -247,13 +247,13 @@ export class ComputedNode extends Node {
     return !agentFilter.agentIds && !agentFilter.nodeIds;
   }
 
-  private agentFilterHandler(context: AgentFunctionContext, agentFunction: AgentFunction): Promise<ResultData> {
+  private agentFilterHandler(context: AgentFunctionContext, agentFunction: AgentFunction, agentId?: string): Promise<ResultData> {
     let index = 0;
 
     const next = (innerContext: AgentFunctionContext): Promise<ResultData> => {
       const agentFilter = this.graph.agentFilters[index++];
       if (agentFilter) {
-        if (this.shouldApplyAgentFilter(agentFilter)) {
+        if (this.shouldApplyAgentFilter(agentFilter, agentId)) {
           if (agentFilter.filterParams) {
             innerContext.filterParams = { ...agentFilter.filterParams, ...innerContext.filterParams };
           }
@@ -277,6 +277,7 @@ export class ComputedNode extends Node {
       return;
     }
     const previousResults = this.graph.resultsOf(this.inputs, this.anyInput);
+    const agentId = this.agentId;
     const transactionId = Date.now();
     this.prepareExecute(transactionId, Object.values(previousResults));
 
@@ -287,9 +288,9 @@ export class ComputedNode extends Node {
     }
 
     try {
-      const agentFunction = this.agentFunction ?? this.graph.getAgentFunctionInfo(this.agentId).agent;
+      const agentFunction = this.agentFunction ?? this.graph.getAgentFunctionInfo(agentId).agent;
       const localLog: TransactionLog[] = [];
-      const context = this.getContext(previousResults, localLog);
+      const context = this.getContext(previousResults, localLog, agentId);
 
       // NOTE: We use the existence of graph object in the agent-specific params to determine
       // if this is a nested agent or not.
@@ -310,7 +311,7 @@ export class ComputedNode extends Node {
       }
 
       this.beforeConsoleLog(context);
-      const result = await this.agentFilterHandler(context as AgentFunctionContext, agentFunction);
+      const result = await this.agentFilterHandler(context as AgentFunctionContext, agentFunction, agentId);
       this.afterConsoleLog(result);
 
       if (this.nestedGraph) {
@@ -375,13 +376,13 @@ export class ComputedNode extends Node {
     }
   }
 
-  private getContext(previousResults: Record<string, ResultData | undefined>, localLog: TransactionLog[]) {
+  private getContext(previousResults: Record<string, ResultData | undefined>, localLog: TransactionLog[], agentId?: string) {
     const context: AgentFunctionContext<DefaultParamsType, DefaultInputData | string | number | boolean | undefined> = {
       params: this.graph.resultsOf(this.params),
       namedInputs: previousResults,
-      inputSchema: this.agentFunction ? undefined : this.graph.getAgentFunctionInfo(this.agentId)?.inputs,
-      debugInfo: this.getDebugInfo(),
-      cacheType: this.agentFunction ? undefined : this.graph.getAgentFunctionInfo(this.agentId)?.cacheType,
+      inputSchema: this.agentFunction ? undefined : this.graph.getAgentFunctionInfo(agentId)?.inputs,
+      debugInfo: this.getDebugInfo(agentId),
+      cacheType: this.agentFunction ? undefined : this.graph.getAgentFunctionInfo(agentId)?.cacheType,
       filterParams: this.filterParams,
       agentFilters: this.graph.agentFilters,
       config: this.graph.config,
@@ -401,10 +402,10 @@ export class ComputedNode extends Node {
     return result;
   }
 
-  private getDebugInfo() {
+  private getDebugInfo(agentId?: string) {
     return {
       nodeId: this.nodeId,
-      agentId: this.agentId,
+      agentId,
       retry: this.retryCount,
       verbose: this.graph.verbose,
       version: this.graph.version,
