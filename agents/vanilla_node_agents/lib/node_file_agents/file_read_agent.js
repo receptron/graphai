a8d2b9914c02ac5728e71e0a85434b64476f4333
isamu <isamu@to-kyo.to>
2024-12-27 08:30:25 +0900

    @graphai/vanilla_node_agents@0.2.7

--- agents/vanilla_node_agents/lib/node_file_agents/file_read_agent.js
+++ agents/vanilla_node_agents/lib/node_file_agents/file_read_agent.js
@@ -12,6 +12,9 @@ const fileReadAgent = async ({ namedInputs, params }) => {
     (0, graphai_1.assert)(!!baseDir, "fileReadAgent: params.baseDir is UNDEFINED!");
     const fileToData = (fileName) => {
         const file = path_1.default.resolve(path_1.default.join(baseDir, fileName));
+        if (outputType && outputType === "stream") {
+            return fs_1.default.createReadStream(file);
+        }
         const buffer = fs_1.default.readFileSync(file);
         if (outputType && outputType === "base64") {
             return buffer.toString("base64");
