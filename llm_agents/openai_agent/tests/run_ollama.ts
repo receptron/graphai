031d1a5447433f1ec6085f663d66d21460f32cd3
isamu <isamu@to-kyo.to>
2025-01-08 07:58:15 +0900

    update test

--- llm_agents/openai_agent/tests/run_ollama.ts
+++ llm_agents/openai_agent/tests/run_ollama.ts
@@ -6,15 +6,19 @@ import assert from "node:assert";
 
 test("test oolama", async () => {
   const namedInputs = { prompt: ["hello, let me know the answer 1 + 1"] };
-  const model = "gemma";
-  // const model = "llama3";
+  
+  // const model = "gemma";
+  const model = "llama3";
   // const model = "phi3";
   const params = {
     baseURL: "http://127.0.0.1:11434/v1",
-    model,
-    apiKey: model,
+    // apiKey: model,
   };
-  const res = (await openAIAgent({ namedInputs, params, filterParams: {}, debugInfo: { verbose: false, nodeId: "test", retry: 5 } })) as any;
+  const config = {
+    model,
+  };
+  
+  const res = (await openAIAgent({ namedInputs, params, filterParams: {}, debugInfo: { verbose: false, nodeId: "test", retry: 5 }, config })) as any;
   if (res) {
     console.log(res.choices[0].message["content"]);
   }
