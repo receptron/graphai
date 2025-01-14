13c95cef95a19b462a1ae0a20e4a21423d45621a
isamu <isamu@to-kyo.to>
2025-01-15 07:40:38 +0900

    nestedAgentGenerator

--- agents/vanilla_agents/src/graph_agents/nested_agent.ts
+++ agents/vanilla_agents/src/graph_agents/nested_agent.ts
@@ -1,57 +1,68 @@
-import { GraphAI, AgentFunction, AgentFunctionInfo, StaticNodeData, assert, graphDataLatestVersion } from "graphai";
+import type { AgentFunction, AgentFunctionInfo, AgentFunctionContext, StaticNodeData, GraphData } from "graphai";
+import { GraphAI, assert, graphDataLatestVersion } from "graphai";
 
-export const nestedAgent: AgentFunction<{ throwError?: boolean }> = async ({ namedInputs, log, debugInfo, params, forNestedGraph }) => {
-  assert(!!forNestedGraph, "Please update graphai to 0.5.19 or higher");
+export const nestedAgentGenerator: (graphData: GraphData) => (context: AgentFunctionContext) => Promise<any> = (graphData: GraphData) => {
+  return async (context: AgentFunctionContext) => {
+    const { namedInputs, log, debugInfo, params, forNestedGraph } = context;
+    assert(!!forNestedGraph, "Please update graphai to 0.5.19 or higher");
 
-  const { agents, graphData, graphOptions, onLogCallback } = forNestedGraph;
-  const { taskManager } = graphOptions;
-  const throwError = params.throwError ?? false;
-  if (taskManager) {
-    const status = taskManager.getStatus(false);
-    assert(status.concurrency > status.running, `nestedAgent: Concurrency is too low: ${status.concurrency}`);
-  }
-  assert(!!graphData, "nestedAgent: graph is required");
+    const { agents, graphOptions, onLogCallback } = forNestedGraph;
+    const { taskManager } = graphOptions;
+    const throwError = params.throwError ?? false;
+    if (taskManager) {
+      const status = taskManager.getStatus(false);
+      assert(status.concurrency > status.running, `nestedAgent: Concurrency is too low: ${status.concurrency}`);
+    }
+    assert(!!graphData, "nestedAgent: graph is required");
 
-  const { nodes } = graphData;
-  const nestedGraphData = { ...graphData, nodes: { ...nodes }, version: graphDataLatestVersion }; // deep enough copy
+    const { nodes } = graphData;
+    const nestedGraphData = { ...graphData, nodes: { ...nodes }, version: graphDataLatestVersion }; // deep enough copy
 
-  const nodeIds = Object.keys(namedInputs);
-  if (nodeIds.length > 0) {
-    nodeIds.forEach((nodeId) => {
-      if (nestedGraphData.nodes[nodeId] === undefined) {
-        // If the input node does not exist, automatically create a static node
-        nestedGraphData.nodes[nodeId] = { value: namedInputs[nodeId] };
-      } else {
-        // Otherwise, inject the proper data here (instead of calling injectTo method later)
-        (nestedGraphData.nodes[nodeId] as StaticNodeData)["value"] = namedInputs[nodeId];
+    const nodeIds = Object.keys(namedInputs);
+    if (nodeIds.length > 0) {
+      nodeIds.forEach((nodeId) => {
+        if (nestedGraphData.nodes[nodeId] === undefined) {
+          // If the input node does not exist, automatically create a static node
+          nestedGraphData.nodes[nodeId] = { value: namedInputs[nodeId] };
+        } else {
+          // Otherwise, inject the proper data here (instead of calling injectTo method later)
+          (nestedGraphData.nodes[nodeId] as StaticNodeData)["value"] = namedInputs[nodeId];
+        }
+      });
+    }
+
+    try {
+      if (nestedGraphData.version === undefined && debugInfo.version) {
+        nestedGraphData.version = debugInfo.version;
       }
-    });
-  }
+      const graphAI = new GraphAI(nestedGraphData, agents || {}, graphOptions);
+      // for backward compatibility. Remove 'if' later
+      if (onLogCallback) {
+        graphAI.onLogCallback = onLogCallback;
+      }
 
-  try {
-    if (nestedGraphData.version === undefined && debugInfo.version) {
-      nestedGraphData.version = debugInfo.version;
+      const results = await graphAI.run(false);
+      log?.push(...graphAI.transactionLogs());
+      return results;
+    } catch (error) {
+      if (error instanceof Error && !throwError) {
+        return {
+          onError: {
+            message: error.message,
+            error,
+          },
+        };
+      }
+      throw error;
     }
-    const graphAI = new GraphAI(nestedGraphData, agents || {}, graphOptions);
-    // for backward compatibility. Remove 'if' later
-    if (onLogCallback) {
-      graphAI.onLogCallback = onLogCallback;
-    }
+  };
+};
 
-    const results = await graphAI.run(false);
-    log?.push(...graphAI.transactionLogs());
-    return results;
-  } catch (error) {
-    if (error instanceof Error && !throwError) {
-      return {
-        onError: {
-          message: error.message,
-          error,
-        },
-      };
-    }
-    throw error;
-  }
+export const nestedAgent: AgentFunction<{ throwError?: boolean }> = async (context) => {
+  const { forNestedGraph } = context;
+  const { graphData } = forNestedGraph ?? { graphData: { nodes: {} } };
+
+  return await nestedAgentGenerator(graphData)(context);
 };
 
 const nestedAgentInfo: AgentFunctionInfo = {
--- agents/vanilla_agents/tests/agents/test_nest_agent.ts
+++ agents/vanilla_agents/tests/agents/test_nest_agent.ts
@@ -1,4 +1,6 @@
 import { nestedAgent } from "@/index";
+import { nestedAgentGenerator } from "@/graph_agents/nested_agent";
+
 import { sleepAndMergeAgent } from "@graphai/sleeper_agents";
 import { defaultTestContext, graphDataLatestVersion } from "graphai";
 
@@ -21,6 +23,33 @@ test("test nest agent", async () => {
           },
         },
       },
+    },
+    namedInputs: { prop1: { apple: "red" }, prop2: { lemon: "yellow" }, prop3: { orange: "orange" } },
+  });
+  assert.deepStrictEqual(result, {
+    node1: { apple: "red", lemon: "yellow", orange: "orange" },
+  });
+});
+
+test("test nest agent generator", async () => {
+  const graphData = {
+    version: graphDataLatestVersion,
+    nodes: {
+      node1: {
+        agent: "sleepAndMergeAgent",
+        inputs: { array: [":prop1", ":prop2", ":prop3"] },
+        isResult: true,
+      },
+    },
+  };
+  const testAgent = nestedAgentGenerator(graphData);
+
+  const result = await testAgent({
+    ...defaultTestContext,
+    forNestedGraph: {
+      agents: { sleepAndMergeAgent },
+      graphOptions: {},
+      graphData: { version: 0, nodes: {} }, // dummy
     },
     namedInputs: { prop1: { apple: "red" }, prop2: { lemon: "yellow" }, prop3: { orange: "orange" } },
   });
