9e613690587d8abab6e383b8b4bf755e0de5e594
isamu <isamu@to-kyo.to>
2025-01-23 06:47:02 +0900

    fix stringUpdateTextGraph: Allow undefined inputs

--- agents/vanilla_agents/src/string_agents/string_update_text_agent.ts
+++ agents/vanilla_agents/src/string_agents/string_update_text_agent.ts
@@ -4,6 +4,12 @@ import { graphDataLatestVersion, AgentFunctionInfo } from "graphai";
 const stringUpdateTextGraph = {
   version: graphDataLatestVersion,
   nodes: {
+    newText: {
+      value: "",
+    },
+    oldText: {
+      value: "",
+    },
     isNewText: {
       if: ":newText",
       agent: "copyAgent",
@@ -50,6 +56,16 @@ const stringUpdateTextAgentInfo: AgentFunctionInfo = {
     },
     {
       inputs: { newText: "", oldText: "old" },
+      params: {},
+      result: { text: "old" },
+    },
+    {
+      inputs: {},
+      params: {},
+      result: { text: "" },
+    },
+    {
+      inputs: { oldText: "old" },
       params: {},
       result: { text: "old" },
     },
