20044555f7210ac0aa5c3116fd947d01859c3edd
isamu <isamu@to-kyo.to>
2025-02-24 18:48:14 +0900

    @graphai/data_agents@0.2.1

--- agents/data_agents/lib/data_object_merge_template_agent.js
+++ agents/data_agents/lib/data_object_merge_template_agent.js
@@ -5,10 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dataObjectMergeTemplateAgent = void 0;
 const deepmerge_1 = __importDefault(require("deepmerge"));
-const dataObjectMergeTemplateAgent = async ({ namedInputs }) => {
-    return namedInputs.array.reduce((tmp, input) => {
+const dataObjectMergeTemplateAgent = async ({ namedInputs, params, }) => {
+    const { flatResponse } = params;
+    const data = namedInputs.array.reduce((tmp, input) => {
         return (0, deepmerge_1.default)(tmp, input);
     }, {});
+    if (flatResponse) {
+        return data;
+    }
+    return { data };
 };
 exports.dataObjectMergeTemplateAgent = dataObjectMergeTemplateAgent;
 // for test and document
@@ -28,7 +33,7 @@ const dataObjectMergeTemplateAgentInfo = {
     samples: [
         {
             inputs: { array: [{ content1: "hello" }, { content2: "test" }] },
-            params: {},
+            params: { flatResponse: true },
             result: {
                 content1: "hello",
                 content2: "test",
@@ -36,31 +41,79 @@ const dataObjectMergeTemplateAgentInfo = {
         },
         {
             inputs: { array: [{ content1: "hello" }] },
-            params: {},
+            params: { flatResponse: true },
             result: {
                 content1: "hello",
             },
         },
         {
             inputs: { array: [{ content: "hello1" }, { content: "hello2" }] },
-            params: {},
+            params: { flatResponse: true },
             result: {
                 content: "hello2",
             },
         },
         {
             inputs: sampleInputs,
-            params: sampleParams,
+            params: { flatResponse: true },
             result: sampleResult,
         },
         {
             inputs: { array: [{ a: { b: { c: { d: "e" } } } }, { b: { c: { d: { e: "f" } } } }, { b: { d: { e: { f: "g" } } } }] },
-            params: {},
+            params: { flatResponse: true },
             result: {
                 a: { b: { c: { d: "e" } } },
                 b: {
                     c: { d: { e: "f" } },
                     d: { e: { f: "g" } },
+                },
+            },
+        },
+        {
+            inputs: { array: [{ content1: "hello" }, { content2: "test" }] },
+            params: {},
+            result: {
+                data: {
+                    content1: "hello",
+                    content2: "test",
+                },
+            },
+        },
+        {
+            inputs: { array: [{ content1: "hello" }] },
+            params: {},
+            result: {
+                data: {
+                    content1: "hello",
+                },
+            },
+        },
+        {
+            inputs: { array: [{ content: "hello1" }, { content: "hello2" }] },
+            params: {},
+            result: {
+                data: {
+                    content: "hello2",
+                },
+            },
+        },
+        {
+            inputs: sampleInputs,
+            params: sampleParams,
+            result: {
+                data: sampleResult,
+            },
+        },
+        {
+            inputs: { array: [{ a: { b: { c: { d: "e" } } } }, { b: { c: { d: { e: "f" } } } }, { b: { d: { e: { f: "g" } } } }] },
+            params: {},
+            result: {
+                data: {
+                    a: { b: { c: { d: "e" } } },
+                    b: {
+                        c: { d: { e: "f" } },
+                        d: { e: { f: "g" } },
+                    },
                 },
             },
         },
