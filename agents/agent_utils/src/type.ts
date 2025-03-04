775920d3d32c5d516b60015dccc266c90eae529a
isamu <isamu@to-kyo.to>
2025-03-04 09:24:37 +0900

    add array type

---
+++ agents/agent_utils/src/type.ts
@@ -0,0 +1,22 @@
+// for Array
+export type GraphAIArray<Item = unknown> = { array: Array<Item> };
+export type GraphAIItem<Item = unknown> = { item: Item };
+export type GraphAIItems<Item = unknown> = { items: Array<Item> };
+
+// array + item
+export type GraphAIArrayWithItem<Item = unknown> = GraphAIArray<Item> & GraphAIItem<Item>;
+
+// array + items
+export type GraphAIArrayWithItems<Item = unknown> = GraphAIArray<Item> & GraphAIItems<Item>;
+
+// array + item?
+export type GraphAIArrayWithOptionalItem<Item = unknown> = GraphAIArray<Item> & Partial<GraphAIItem<Item>>;
+
+// array + items?
+export type GraphAIArrayWithOptionalItems<Item = unknown> = GraphAIArray<Item> & Partial<GraphAIItems<Item>>;
+
+// array + item? + items?
+export type GraphAIArrayWithOptionalItemAndItems<Item = unknown> = GraphAIArray<Item> & Partial<GraphAIItem<Item>> & Partial<GraphAIItems<Item>>;
+
+// array + item + items
+export type GraphAIArrayWithItemAndItems<Item = unknown> = GraphAIArray<Item> & GraphAIItem<Item> & GraphAIItems<Item>;
