2b7fab4dfe6cc25c22e76ae409ad102de0ea445b
isamu <isamu@to-kyo.to>
2025-04-26 20:37:39 +0900

    add merge_objects_agent

--- agents/vanilla_agents/src/data_agents/index.ts
+++ agents/vanilla_agents/src/data_agents/index.ts
@@ -3,5 +3,6 @@ import dataSumTemplateAgent from "./data_sum_template_agent";
 import propertyFilterAgent from "./property_filter_agent";
 import copyAgent from "./copy_agent";
 import lookupDictionaryAgent from "./lookup_dictionary_agent";
+import mergeObjectAgentInfo from "./merge_objects_agent";
 
-export { totalAgent, dataSumTemplateAgent, propertyFilterAgent, copyAgent, lookupDictionaryAgent };
+export { totalAgent, dataSumTemplateAgent, propertyFilterAgent, copyAgent, lookupDictionaryAgent, mergeObjectAgentInfo };
