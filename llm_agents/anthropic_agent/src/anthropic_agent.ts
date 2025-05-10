512ac6c4a9f8602d8e2cf9f46b8fba296887a7b1
isamu <isamu@to-kyo.to>
2025-05-11 05:16:11 +0900

    fix type

--- llm_agents/anthropic_agent/src/anthropic_agent.ts
+++ llm_agents/anthropic_agent/src/anthropic_agent.ts
@@ -67,7 +67,7 @@ export const anthropicAgent: AgentFunction<AnthropicParams, AnthropicResult, Ant
   const systemPrompt = getMergeValue(namedInputs, params, "mergeableSystem", system);
 
   const messagesCopy: Array<Anthropic.MessageParam> = messages ? messages.map((m) => m) : [];
-  const messageSystemPrompt = messagesCopy[0] && messagesCopy[0].role === "system" ? messagesCopy[0].content : null;
+  const messageSystemPrompt = messagesCopy[0] && (messagesCopy[0] as any).role === "system" ? (messagesCopy[0].content as string) : "";
 
   if (userPrompt) {
     messagesCopy.push({
@@ -95,7 +95,7 @@ export const anthropicAgent: AgentFunction<AnthropicParams, AnthropicResult, Ant
   const anthropic = new Anthropic({ apiKey, dangerouslyAllowBrowser: !!forWeb });
   const chatParams = {
     model: model ?? "claude-3-5-sonnet-latest",
-    messages: messagesCopy.filter((m) => m.role !== "system"),
+    messages: messagesCopy.filter((m) => (m.role as any) !== "system"),
     tools: anthropic_tools,
     tool_choice,
     system: systemPrompt || messageSystemPrompt,
