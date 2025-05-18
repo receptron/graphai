4866db1e20786dbb65c1d26e9abdf361cffbbe58
isamu <isamu@to-kyo.to>
2025-05-18 19:49:23 +0900

    add files

---
+++ agent_filters/stream_agent_filter/src/stream.ts
@@ -0,0 +1,16 @@
+import type { AgentFilterFunction, AgentFunctionContext } from "graphai";
+import { NodeState } from "graphai";
+
+export const streamAgentFilterGenerator = <T>(callback: (context: AgentFunctionContext, data: T) => void) => {
+  const streamAgentFilter: AgentFilterFunction = async (context, next) => {
+    if (context.debugInfo.isResult) {
+      context.filterParams.streamDataCallback = (data: T) => {
+        if (context.debugInfo.state === NodeState.Executing) {
+          callback(context, data);
+        }
+      };
+    }
+    return next(context);
+  };
+  return streamAgentFilter;
+};
