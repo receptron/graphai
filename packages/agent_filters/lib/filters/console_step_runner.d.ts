3a49fea59c2d74d51c32c4cdfe2e2f1fb1b2a312
isamu <isamu@to-kyo.to>
2025-05-18 21:09:41 +0900

    update and build

--- packages/agent_filters/lib/filters/console_step_runner.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const consoleStepRunner: import("graphai").AgentFilterFunction;
--- packages/agent_filters/lib/filters/step_runner_generator.d.ts
+++
@@ -1,3 +0,0 @@
-import type { AgentFunctionContext, AgentFilterFunction } from "graphai";
-export type StepRunnerAwaitFunction = (context: AgentFunctionContext, result: unknown) => Promise<void>;
-export declare const stepRunnerGenerator: (awaitStep: StepRunnerAwaitFunction) => AgentFilterFunction;
