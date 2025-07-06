2bb8842fd40277129feeba9dee3820c8e74dcd96
isamu <isamu@to-kyo.to>
2025-07-07 06:52:17 +0900

    more test

--- packages/graphai/tests/props_functions/test_props_function_undefined.ts
+++ packages/graphai/tests/props_functions/test_props_function_undefined.ts
@@ -5,14 +5,27 @@ import { getDataFromSource } from "../../src/utils/data_source";
 import test from "node:test";
 import assert from "node:assert";
 
-// number function
-test("test getDataFromSource number", async () => {
+// undefined function
+test("test getDataFromSource undefined", async () => {
   const inputId = ":node1.data.default(123)";
   const result = { data: undefined };
   const data = 123;
 
   const source = parseNodeName(inputId);
   assert.deepStrictEqual(source, { nodeId: "node1", propIds: ["data", "default(123)"] });
+
+  const res = getDataFromSource(result, source, propFunctions);
+  assert.deepStrictEqual(res, data);
+});
+
+
+test("test getDataFromSource undefined", async () => {
+  const inputId = ":node1.data.default(abc)";
+  const result = { data: undefined };
+  const data = "abc";
+
+  const source = parseNodeName(inputId);
+  assert.deepStrictEqual(source, { nodeId: "node1", propIds: ["data", "default(abc)"] });
 
   const res = getDataFromSource(result, source, propFunctions);
   assert.deepStrictEqual(res, data);
