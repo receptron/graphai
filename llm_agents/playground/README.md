d600ff01e65bc8a34dc1f35e2ea6ffb9df896019
isamu <isamu@to-kyo.to>
2025-08-18 07:48:11 +0900

    add playGround

---
+++ llm_agents/playground/README.md
@@ -0,0 +1,6 @@
+
+Anthropic tools
+```
+node --test  --require ts-node/register  src/tools/run_tools.ts
+``` 
+
---
+++ llm_agents/playground/eslint.config.mjs
@@ -0,0 +1,11 @@
+import eslintBase from "../../config/eslint.config.base.mjs";
+
+export default [
+  {
+    files: ["{src,test,samples}/**/*.{js,ts,yaml,yml,json}"],
+  },
+  {
+    ignores: ["lib/**/*", "*.ts", "apiDoc/**/*"],
+  },
+  ...eslintBase,
+];
